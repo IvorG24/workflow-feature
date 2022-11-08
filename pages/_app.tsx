e7d8d08ce0f8bbeebfbc1f2cf1367185c812678b
Renzo Visperas <renzovisperas07@gmail.com>
2022-11-08 13:39:09 +0800

    add mantine

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,18 +3,28 @@ import { MantineProvider } from "@mantine/core";
 import { Session, SessionContextProvider } from "@supabase/auth-helpers-react";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
+import createClient from "../utils/supabase";
+import { MantineProvider } from "@mantine/core";
+import mantineTheme from "utils/mantineTheme";
 
 export default function App({
   Component,
   pageProps,
 }: AppProps<{ initialSession: Session }>) {
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      withCSSVariables
+      theme={mantineTheme}
+    >
       <SessionContextProvider
         supabaseClient={createClient}
         initialSession={pageProps.initialSession}
       >
-        <Component {...pageProps} />;
+        <Component {...pageProps} />
       </SessionContextProvider>
     </MantineProvider>
   );
---
+++ utils/mantineTheme.ts
@@ -0,0 +1,10 @@
+// https://mantine.dev/theming/theme-object/
+
+import { MantineThemeOverride } from "@mantine/core";
+
+export const theme: MantineThemeOverride = {
+  colorScheme: "light",
+  primaryColor: "green",
+};
+
+export default theme;
