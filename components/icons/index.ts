6824b4b8f2f0ea7cf580105504202c54e9fc2cf9
Renzo Visperas <renzovisperas07@gmail.com>
2022-11-09 12:49:04 +0800

    add svgr configuration

---
+++ components/icons/index.ts
@@ -0,0 +1,2 @@
+export { default as MoonStars } from "./MoonStars";
+export { default as Sun } from "./Sun";
--- layouts/WorkspaceLayout/components/Header.tsx
+++ layouts/WorkspaceLayout/components/Header.tsx
@@ -1,7 +1,8 @@
 import { Header as MantineHeader } from "@mantine/core";
 import Image from "next/image";
-import { Container, Group } from "@mantine/core";
+import { Container, Group, Button } from "@mantine/core";
 import styles from "./Header.module.scss";
+import { Sun } from "components/icons";
 
 const Header = () => {
   return (
@@ -9,6 +10,16 @@ const Header = () => {
       <Container fluid m={0}>
         <Group align="center">
           <Image src="/images/logo.png" alt="Logo" width={150} height={48} />
+          <Button
+            variant="default"
+            size="xs"
+            px="xs"
+            className={styles.darkmodeToggle}
+          >
+            <div className={styles.icon}>
+              <Sun />
+            </div>
+          </Button>
         </Group>
       </Container>
     </MantineHeader>
