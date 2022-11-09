d37995ad93366232ccf2da402257fa6477e5641a
Renzo Visperas <renzovisperas07@gmail.com>
2022-11-09 11:19:56 +0800

    add logo and sass

--- layouts/WorkspaceLayout/components/Header.tsx
+++ layouts/WorkspaceLayout/components/Header.tsx
@@ -1,7 +1,18 @@
 import { Header as MantineHeader } from "@mantine/core";
+import Image from "next/image";
+import { Container, Group } from "@mantine/core";
+import styles from "./Header.module.scss";
 
 const Header = () => {
-  return <MantineHeader height={60}>asdasd</MantineHeader>;
+  return (
+    <MantineHeader height={60} className={styles.header}>
+      <Container fluid m={0}>
+        <Group align="center">
+          <Image src="/images/logo.png" alt="Logo" width={150} height={48} />
+        </Group>
+      </Container>
+    </MantineHeader>
+  );
 };
 
 export default Header;
---
+++ pages/test.tsx
@@ -0,0 +1,11 @@
+import WorkspaceLayout from "layouts/WorkspaceLayout/WorkspaceLayout";
+
+const test = () => {
+  return (
+    <WorkspaceLayout>
+      <div>Test</div>
+    </WorkspaceLayout>
+  );
+};
+
+export default test;
