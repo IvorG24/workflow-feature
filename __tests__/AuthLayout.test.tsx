0a651c0641ea82182439b21a1e2057160cef163c
Mark Jay Lunas <you@exagit config --global user.email markjay.lunas@gmail.com git config --global user.email you@exa>
2022-11-22 21:28:34 +0800

    Add auth layout test

---
+++ __tests__/AuthLayout.test.tsx
@@ -0,0 +1,25 @@
+import AuthLayout from "@/components/Layout/AuthLayout";
+import { render, screen } from "@testing-library/react";
+
+const setup = () => {
+  render(<AuthLayout>children</AuthLayout>);
+};
+
+describe("auth layout", () => {
+  it("renders a logo", () => {
+    setup();
+    const logo = screen.getByTestId("logo");
+    expect(logo).toBeInTheDocument();
+  });
+  it("renders a heading", () => {
+    setup();
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+  });
+
+  it("renders a description", () => {
+    setup();
+    const description = screen.getByTestId("description");
+    expect(description).toBeInTheDocument();
+  });
+});
