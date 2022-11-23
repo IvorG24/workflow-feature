06f9750bef69e03da0a48af4f54b88d01f4fb3f3
RansuAndorei <lancejuat26@gmail.com>
2022-11-23 14:36:58 +0800

    add redirection

--- components/templates/Settings/Member/YourTeamSection/MembersTable/MembersTable.tsx
+++ components/templates/Settings/Member/YourTeamSection/MembersTable/MembersTable.tsx
@@ -1,4 +1,5 @@
 import { Avatar, Select, Text, Title } from "@mantine/core";
+import { useRouter } from "next/router";
 import { Member } from "../../Member";
 import styles from "./MembersTable.module.scss";
 
@@ -7,12 +8,15 @@ type Props = {
 };
 
 const MembersTable = ({ filteredMembers }: Props) => {
+  const router = useRouter();
+
   return (
     <div className={styles.tableWrapper}>
       {filteredMembers.map(({ name, id, email, role, image }) => {
         return (
           <div
             key={id}
+            onClick={(e) => router.push(`/member/${id}`)}
             className={styles.desktopOnlyRow}
             data-testid="membersRow"
           >
@@ -33,6 +37,7 @@ const MembersTable = ({ filteredMembers }: Props) => {
                   { value: "admin", label: "Admin" },
                   { value: "owner", label: "Owner" },
                 ]}
+                onClick={(e) => e.stopPropagation()}
                 value={role}
               />
             </div>
