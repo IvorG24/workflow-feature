a7722cd149e976f06f3cf4eab3ea5f1c14e8e719
Mark Jay Lunas <you@exagit config --global user.email markjay.lunas@gmail.com git config --global user.email you@exa>
2022-11-23 14:35:05 +0800

    Refactor auth layout to use one component only

--- components/Layout/AuthLayout.tsx
+++ components/Layout/AuthLayout.tsx
@@ -1,7 +1,15 @@
-import { MantineProvider } from "@mantine/core";
+import {
+  Container,
+  Flex,
+  MantineProvider,
+  Stack,
+  Text,
+  Title,
+} from "@mantine/core";
 import { useColorScheme } from "@mantine/hooks";
 import { ReactNode } from "react";
-import Auth from "../Auth/Auth";
+import { Logo } from "../Icon";
+import styles from "./AuthLayout.module.scss";
 
 type Props = {
   children: ReactNode;
@@ -11,7 +19,23 @@ const AuthLayout = ({ children }: Props) => {
   const colorScheme = useColorScheme();
   return (
     <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
-      <Auth>{children}</Auth>
+      <Flex styles={styles.auth}>
+        <Container className={styles.welcome}>
+          <Stack w={300}>
+            <div className={styles.logo} data-testid="logo">
+              <Logo />
+            </div>
+            <Title order={1} color="dark.6">
+              Welcome to Formsly
+            </Title>
+            <Text color="gray.7">
+              We help businesses automate all their requests and processes with
+              their teams
+            </Text>
+          </Stack>
+        </Container>
+        {children}
+      </Flex>
     </MantineProvider>
   );
 };
