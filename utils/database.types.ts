e4eefec530c9d05099a3d34dc1d070f4b1679bea
Albert Linao <5449869+alberto-linao@users.noreply.github.com>
2022-11-23 11:07:20 +0800

    Update database.types.ts

--- utils/database.types.ts
+++ utils/database.types.ts
@@ -9,23 +9,143 @@ export type Json =
 export type Database = {
   public: {
     Tables: {
-      user_profile: {
+      form_name_table: {
         Row: {
-          id: string;
+          form_name_id: number;
+          form_name: string | null;
+        };
+        Insert: {
+          form_name_id?: never;
+          form_name?: string | null;
+        };
+        Update: {
+          form_name_id?: never;
+          form_name?: string | null;
+        };
+      };
+      form_priority_table: {
+        Row: {
+          form_name_id: number;
+          priority: number[] | null;
+        };
+        Insert: {
+          form_name_id: number;
+          priority?: number[] | null;
+        };
+        Update: {
+          form_name_id?: number;
+          priority?: number[] | null;
+        };
+      };
+      form_table: {
+        Row: {
+          form_id: number;
+          form_name_id: number | null;
+          created_at: string | null;
+          question_id: number | null;
+          response_value: string | null;
+          response_owner: string | null;
+          response_comment: string | null;
+          team_id: number | null;
+        };
+        Insert: {
+          form_id?: never;
+          form_name_id?: number | null;
+          created_at?: string | null;
+          question_id?: number | null;
+          response_value?: string | null;
+          response_owner?: string | null;
+          response_comment?: string | null;
+          team_id?: number | null;
+        };
+        Update: {
+          form_id?: never;
+          form_name_id?: number | null;
+          created_at?: string | null;
+          question_id?: number | null;
+          response_value?: string | null;
+          response_owner?: string | null;
+          response_comment?: string | null;
+          team_id?: number | null;
+        };
+      };
+      question_table: {
+        Row: {
+          question_id: number;
+          question: string | null;
+          expected_response_type:
+            | Database["public"]["Enums"]["expected_response_type"]
+            | null;
+        };
+        Insert: {
+          question_id?: never;
+          question?: string | null;
+          expected_response_type?:
+            | Database["public"]["Enums"]["expected_response_type"]
+            | null;
+        };
+        Update: {
+          question_id?: never;
+          question?: string | null;
+          expected_response_type?:
+            | Database["public"]["Enums"]["expected_response_type"]
+            | null;
+        };
+      };
+      team_role_table: {
+        Row: {
+          team_id: number;
+          user_id: string;
+          team_role: Database["public"]["Enums"]["team_role"] | null;
+          lock_account: boolean | null;
+        };
+        Insert: {
+          team_id: number;
+          user_id: string;
+          team_role?: Database["public"]["Enums"]["team_role"] | null;
+          lock_account?: boolean | null;
+        };
+        Update: {
+          team_id?: number;
+          user_id?: string;
+          team_role?: Database["public"]["Enums"]["team_role"] | null;
+          lock_account?: boolean | null;
+        };
+      };
+      team_table: {
+        Row: {
+          team_id: number;
+          team_name: string | null;
+          user_id: string | null;
+        };
+        Insert: {
+          team_id?: never;
+          team_name?: string | null;
+          user_id?: string | null;
+        };
+        Update: {
+          team_id?: never;
+          team_name?: string | null;
+          user_id?: string | null;
+        };
+      };
+      user_profile_table: {
+        Row: {
+          user_id: string;
           updated_at: string | null;
           username: string | null;
           full_name: string | null;
           avatar_url: string | null;
         };
         Insert: {
-          id: string;
+          user_id: string;
           updated_at?: string | null;
           username?: string | null;
           full_name?: string | null;
           avatar_url?: string | null;
         };
         Update: {
-          id?: string;
+          user_id?: string;
           updated_at?: string | null;
           username?: string | null;
           full_name?: string | null;
@@ -43,7 +163,17 @@ export type Database = {
       };
     };
     Enums: {
-      [_ in never]: never;
+      expected_response_type:
+        | "text"
+        | "number"
+        | "date"
+        | "daterange"
+        | "time"
+        | "email"
+        | "select"
+        | "slider"
+        | "multiple";
+      team_role: "member" | "manager";
     };
   };
 };
