ad41cb040a3ea237ab864aaaf53aa90756931d04
jason-evangelista <evangelistajasonglenn@gmail.com>
2022-11-26 10:57:05 +0800

    Add create-request-form page

---
+++ pages/create-request-form.tsx
@@ -0,0 +1,36 @@
+import { GetServerSideProps } from "next";
+import { ReactElement } from "react";
+import { resetServerContext } from "react-beautiful-dnd";
+import type { NextPageWithLayout } from "./_app";
+
+import CreateRequestFormPage from "@/components/CreateRequestFormPage/CreateRequestFormPage";
+import WorkspaceLayout from "@/components/Layout/WorkspaceLayout";
+import Meta from "@/components/Meta/Meta";
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  resetServerContext();
+  return {
+    props: {
+      data: [],
+    },
+  };
+};
+
+const CreateRequestForm: NextPageWithLayout = () => {
+  return (
+    <div>
+      <Meta
+        description="Create Request Form"
+        // this is just a temporary url, canoncial url will be set in the future
+        url="localhost:3000/testpage"
+      />
+      <CreateRequestFormPage />
+    </div>
+  );
+};
+
+CreateRequestForm.getLayout = function getLayout(page: ReactElement) {
+  return <WorkspaceLayout>{page}</WorkspaceLayout>;
+};
+
+export default CreateRequestForm;
