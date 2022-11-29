bae8d091640e86f2af09bc1a2cd3aabc2bef61c4
Juan Carlos Lumingkit <juancarloslumingkit@gmail.com>
2022-11-29 13:21:49 +0800

    change team components changes: 1. Move content of YourTeamSection to Member 2. Delete YourTeamSection and its scss module

--- components/Setting/Member.tsx
+++ components/Setting/Member.tsx
@@ -1,6 +1,11 @@
 // todo: create unit tests
+import { Box, Divider, Flex, Text, Title } from "@mantine/core";
+import { lowerCase } from "lodash";
+import { useState } from "react";
 import { MEMBERS } from "tempData";
-import YourTeamSection from "./YourTeamSection";
+import InviteTeamMembersSection from "./InviteTeamMembersSection";
+import MembersTable from "./MembersTable";
+import SearchBar from "./SearchBar";
 
 export type Member = {
   id: string;
@@ -10,10 +15,32 @@ export type Member = {
   image: string;
 };
 
-export type Members = Member[];
-
 const Member = () => {
-  return <YourTeamSection members={MEMBERS} />;
+  const [searchBarValue, setSearchBarValue] = useState("");
+
+  const filteredMembers = MEMBERS.filter((member) =>
+    lowerCase(member.name).includes(searchBarValue)
+  );
+
+  return (
+    <Flex gap="sm" direction={{ base: "column", md: "row" }}>
+      <Box w={{ base: "100%", lg: "50%" }}>
+        <Title order={3}>Your Team Members</Title>
+        <Text>Manage your existing team and change roles/permissions</Text>
+      </Box>
+      <Box w="100%" ml={{ lg: "10rem" }}>
+        <SearchBar
+          onChange={(e) => setSearchBarValue(e.target.value)}
+          onClear={() => setSearchBarValue("")}
+          value={searchBarValue}
+          numberOfMembers={MEMBERS.length}
+        />
+        <MembersTable filteredMembers={filteredMembers} />
+        <Divider my={{ base: 10, lg: 50 }} />
+        <InviteTeamMembersSection members={MEMBERS} />
+      </Box>
+    </Flex>
+  );
 };
 
 export default Member;
