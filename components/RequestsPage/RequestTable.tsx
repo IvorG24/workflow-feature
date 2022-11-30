b45e318fe9e9c4e30b78bac9c83e37f826f851aa
RansuAndorei <lancejuat26@gmail.com>
2022-11-30 16:19:44 +0800

    remove empty tag

--- components/RequestsPage/RequestTable.tsx
+++ components/RequestsPage/RequestTable.tsx
@@ -104,101 +104,94 @@ const RequestTable = ({
   ));
 
   return (
-    <>
-      <Group align="flex-start">
-        <Table
-          mt="xl"
-          striped
-          highlightOnHover
-          className={styles.tableContainer}
-        >
-          <thead>
-            <tr>
-              <th>REF</th>
-              <th>Request Title</th>
-              <th>Status</th>
-              <th>Last Updated</th>
-              <th>Requested By</th>
-              <th>Approvers</th>
-            </tr>
-          </thead>
-          <tbody>{rows}</tbody>
-        </Table>
+    <Group align="flex-start">
+      <Table mt="xl" striped highlightOnHover className={styles.tableContainer}>
+        <thead>
+          <tr>
+            <th>REF</th>
+            <th>Request Title</th>
+            <th>Status</th>
+            <th>Last Updated</th>
+            <th>Requested By</th>
+            <th>Approvers</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </Table>
 
-        {selectedRequest ? (
-          <Paper shadow="xl" className={styles.requestContainer}>
-            <Container m={0} p={0} className={styles.closeIcon}>
-              <ActionIcon onClick={() => setSelectedRequest(null)}>
-                <Close />
-              </ActionIcon>
-            </Container>
-            <Group position="apart" grow>
-              <Stack align="flex-start">
-                <Title order={5}>Request Title</Title>
-                <Text>{selectedRequest.requestTitle}</Text>
-              </Stack>
-              <Stack align="flex-start">
-                <Title order={5}>Request By</Title>
-                <Group>
-                  <Avatar radius={100} />
-                  <Text>{selectedRequest.requestedBy}</Text>
-                </Group>
-              </Stack>
-            </Group>
-            <Group mt="xl" position="apart" grow>
-              <Stack align="flex-start">
-                <Title order={5}>Date Created</Title>
-                <Text>{selectedRequest.lastUpdated}</Text>
-              </Stack>
-              <Stack align="flex-start">
-                <Title order={5}>Status</Title>
-                <Badge color={setBadgeColor(selectedRequest.status)}>
-                  {startCase(selectedRequest.status)}
-                </Badge>
-              </Stack>
-            </Group>
-            <Stack mt="xl" align="flex-start">
-              <Title order={5}>Request Description</Title>
-              <Text>{selectedRequest.description}</Text>
+      {selectedRequest ? (
+        <Paper shadow="xl" className={styles.requestContainer}>
+          <Container m={0} p={0} className={styles.closeIcon}>
+            <ActionIcon onClick={() => setSelectedRequest(null)}>
+              <Close />
+            </ActionIcon>
+          </Container>
+          <Group position="apart" grow>
+            <Stack align="flex-start">
+              <Title order={5}>Request Title</Title>
+              <Text>{selectedRequest.requestTitle}</Text>
             </Stack>
-            <Divider mt="xl" />
-            <Stack mt="xl">
-              <Title order={5}>Approver</Title>
-              <Group align="apart" grow>
-                {selectedRequest.approvers.map((approver) => (
-                  <Group key={approver.name}>
-                    <Badge color={setBadgeColor(approver.status)} />
-                    <Text>{approver.name}</Text>
-                  </Group>
-                ))}
+            <Stack align="flex-start">
+              <Title order={5}>Request By</Title>
+              <Group>
+                <Avatar radius={100} />
+                <Text>{selectedRequest.requestedBy}</Text>
               </Group>
             </Stack>
-            <Divider mt="xl" />
-            <Stack mt="xl">
-              <Title order={5}>Attachment</Title>
-              <Text>---</Text>
+          </Group>
+          <Group mt="xl" position="apart" grow>
+            <Stack align="flex-start">
+              <Title order={5}>Date Created</Title>
+              <Text>{selectedRequest.lastUpdated}</Text>
             </Stack>
+            <Stack align="flex-start">
+              <Title order={5}>Status</Title>
+              <Badge color={setBadgeColor(selectedRequest.status)}>
+                {startCase(selectedRequest.status)}
+              </Badge>
+            </Stack>
+          </Group>
+          <Stack mt="xl" align="flex-start">
+            <Title order={5}>Request Description</Title>
+            <Text>{selectedRequest.description}</Text>
+          </Stack>
+          <Divider mt="xl" />
+          <Stack mt="xl">
+            <Title order={5}>Approver</Title>
+            <Group align="apart" grow>
+              {selectedRequest.approvers.map((approver) => (
+                <Group key={approver.name}>
+                  <Badge color={setBadgeColor(approver.status)} />
+                  <Text>{approver.name}</Text>
+                </Group>
+              ))}
+            </Group>
+          </Stack>
+          <Divider mt="xl" />
+          <Stack mt="xl">
+            <Title order={5}>Attachment</Title>
+            <Text>---</Text>
+          </Stack>
 
-            {isApprover ? (
-              <>
-                <Divider mt="xl" />
-                <Stack mt="xl">
-                  <Button color="green" onClick={() => handleApprove()}>
-                    Approve
-                  </Button>
-                  <Button color="dark" onClick={() => handleSendToRevision()}>
-                    Send For Revision
-                  </Button>
-                  <Button color="red" onClick={() => handleReject()}>
-                    Reject
-                  </Button>
-                </Stack>
-              </>
-            ) : null}
-          </Paper>
-        ) : null}
-      </Group>
-    </>
+          {isApprover ? (
+            <>
+              <Divider mt="xl" />
+              <Stack mt="xl">
+                <Button color="green" onClick={() => handleApprove()}>
+                  Approve
+                </Button>
+                <Button color="dark" onClick={() => handleSendToRevision()}>
+                  Send For Revision
+                </Button>
+                <Button color="red" onClick={() => handleReject()}>
+                  Reject
+                </Button>
+              </Stack>
+            </>
+          ) : null}
+        </Paper>
+      ) : null}
+    </Group>
   );
 };
 
