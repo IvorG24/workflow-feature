32a546cb0f3bb47a156719cc04683a8f0be394a4
RansuAndorei <lancejuat26@gmail.com>
2022-11-30 16:19:13 +0800

    move pagination

--- components/RequestsPage/All.tsx
+++ components/RequestsPage/All.tsx
@@ -1,7 +1,35 @@
 import { Search } from "@/components/Icon";
-import { ActionIcon, Group, Select, Stack, TextInput } from "@mantine/core";
-import { useState } from "react";
+import {
+  ActionIcon,
+  Group,
+  LoadingOverlay,
+  Pagination,
+  Select,
+  Stack,
+  TextInput,
+} from "@mantine/core";
+import { showNotification } from "@mantine/notifications";
+import { ceil, toLower } from "lodash";
+import { useEffect, useState } from "react";
+import styles from "./All.module.scss";
 import RequestTable from "./RequestTable";
+
+type RequestType = {
+  id: number;
+  ref: string;
+  formType: string;
+  requestTitle: string;
+  status: string;
+  lastUpdated: string;
+  requestedBy: string;
+  description: string;
+  approvers: {
+    name: string;
+    status: string;
+  }[];
+};
+
+const currentUser = "Lance Juat";
 
 const tempFormType = [
   { value: "approvalRequest", label: "Approval Request" },
@@ -22,13 +50,113 @@ const tempStatus = [
   { value: "cancelled", label: "Cancelled" },
 ];
 
+const tempRequests: RequestType[] = [];
+
+const REQUEST_PER_PAGE = 8;
+
 const All = () => {
   const [search, setSearch] = useState("");
   const [formType, setFormType] = useState<string | null>(null);
   const [status, setStatus] = useState<string | null>(null);
+  const [requestList, setRequestList] = useState<RequestType[]>(tempRequests);
+  const [activePage, setPage] = useState(1);
+  const [visibleRequests, setVisibleRequests] = useState(
+    tempRequests.slice(0, REQUEST_PER_PAGE)
+  );
+  const [selectedRequest, setSelectedRequest] = useState<RequestType | null>(
+    null
+  );
+
+  const [isApprover, setIsApprover] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const filterPage = (page: number, requestList: RequestType[]) => {
+    setSelectedRequest(null);
+    setPage(page);
+    setVisibleRequests(
+      requestList.slice(
+        (page - 1) * REQUEST_PER_PAGE,
+        (page - 1) * REQUEST_PER_PAGE + REQUEST_PER_PAGE
+      )
+    );
+  };
+
+  useEffect(() => {
+    setIsApprover(false);
+    if (selectedRequest) {
+      if (
+        selectedRequest.status === "stale" ||
+        selectedRequest.status === "pending"
+      ) {
+        selectedRequest.approvers.map((approver) => {
+          if (approver.name === currentUser && approver.status === "pending") {
+            setIsApprover(true);
+          }
+        });
+      }
+    }
+  }, [selectedRequest]);
+
+  useEffect(() => {
+    let newRequestList: RequestType[] = tempRequests;
+    if (formType) {
+      newRequestList = newRequestList.filter(
+        (request) => request.formType === formType
+      );
+    }
+    if (status) {
+      newRequestList = newRequestList.filter(
+        (request) => request.status === status
+      );
+    }
+    if (search) {
+      newRequestList = newRequestList.filter((request) =>
+        toLower(request.requestTitle).includes(toLower(search))
+      );
+    }
+
+    console.log;
+    setRequestList(newRequestList);
+    filterPage(1, newRequestList);
+  }, [search, formType, status]);
+
+  const handleApprove = () => {
+    setIsLoading(true);
+    showNotification({
+      title: "Success!",
+      message: `You approved ${selectedRequest?.requestTitle}`,
+      color: "green",
+    });
+    setSelectedRequest(null);
+    setIsLoading(false);
+  };
+
+  const handleSendToRevision = () => {
+    setIsLoading(true);
+    showNotification({
+      title: "Success!",
+      message: `${selectedRequest?.requestTitle} is Sent to Revision`,
+      color: "green",
+    });
+    setSelectedRequest(null);
+    setIsLoading(false);
+  };
+
+  const handleReject = () => {
+    setIsLoading(true);
+    showNotification({
+      title: "Success!",
+      message: `You rejected ${selectedRequest?.requestTitle}`,
+      color: "green",
+    });
+    setSelectedRequest(null);
+    setIsLoading(false);
+  };
+
   // todo: add eslint to show error for `mt={"xl"}`
   return (
     <Stack>
+      <LoadingOverlay visible={isLoading} overlayBlur={2} />
       <Group mt="xl">
         <TextInput
           value={search}
@@ -55,7 +183,23 @@ const All = () => {
           onChange={setStatus}
         />
       </Group>
-      <RequestTable formType={formType} status={status} search={search} />
+      <RequestTable
+        visibleRequests={visibleRequests}
+        selectedRequest={selectedRequest}
+        setSelectedRequest={setSelectedRequest}
+        isApprover={isApprover}
+        handleApprove={handleApprove}
+        handleSendToRevision={handleSendToRevision}
+        handleReject={handleReject}
+      />
+      {requestList.length / REQUEST_PER_PAGE > 1 ? (
+        <Pagination
+          className={styles.pagination}
+          page={activePage}
+          onChange={(page) => filterPage(page, requestList)}
+          total={ceil(requestList.length / REQUEST_PER_PAGE)}
+        />
+      ) : null}
     </Stack>
   );
 };
--- components/RequestsPage/RequestTable.tsx
+++ components/RequestsPage/RequestTable.tsx
@@ -8,8 +8,6 @@ import {
   Container,
   Divider,
   Group,
-  LoadingOverlay,
-  Pagination,
   Paper,
   Stack,
   Table,
@@ -18,10 +16,9 @@ import {
   Tooltip,
 } from "@mantine/core";
 import { useViewportSize } from "@mantine/hooks";
-import { showNotification } from "@mantine/notifications";
-import { ceil, startCase, toLower } from "lodash";
+import { startCase } from "lodash";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { Dispatch, SetStateAction } from "react";
 import styles from "./RequestTable.module.scss";
 
 type RequestType = {
@@ -39,269 +36,27 @@ type RequestType = {
   }[];
 };
 
-const currentUser = "Lance Juat";
-
-const tempRequests: RequestType[] = [
-  {
-    id: 1,
-    ref: "AR-1",
-    formType: "approvalRequest",
-    requestTitle: "Approval Request 1",
-    status: "rejected",
-    lastUpdated: "10/19/20",
-    requestedBy: "Lance Juat",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sagittis odio ut bibendum bibendum. Nullam faucibus ex vitae varius molestie. Pellentesque tincidunt sit amet mauris eu malesuada. Etiam auctor ex sed mattis imperdiet. Nulla viverra ullamcorper laoreet. Integer pharetra nec dolor id ornare. Mauris sit amet tincidunt lectus.",
-    approvers: [
-      {
-        name: "Maria Deanna Romero",
-        status: "rejected",
-      },
-    ],
-  },
-  {
-    id: 2,
-    ref: "AR-2",
-    formType: "approvalRequest",
-    requestTitle: "Approval Request 2",
-    status: "approved",
-    lastUpdated: "11/23/20",
-    requestedBy: "James Bautista",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sagittis odio ut bibendum bibendum. Nullam faucibus ex vitae varius molestie. Pellentesque tincidunt sit amet mauris eu malesuada. Etiam auctor ex sed mattis imperdiet. Nulla viverra ullamcorper laoreet. Integer pharetra nec dolor id ornare. Mauris sit amet tincidunt lectus.",
-    approvers: [
-      {
-        name: "Maria Deanna Romero",
-        status: "approved",
-      },
-      {
-        name: "Alberto Luis Linao",
-        status: "approved",
-      },
-    ],
-  },
-  {
-    id: 3,
-    ref: "IT-4803",
-    formType: "it",
-    requestTitle: "IT 4803",
-    status: "approved",
-    lastUpdated: "11/15/21",
-    requestedBy: "Arianne Roxas",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sagittis odio ut bibendum bibendum. Nullam faucibus ex vitae varius molestie. Pellentesque tincidunt sit amet mauris eu malesuada. Etiam auctor ex sed mattis imperdiet. Nulla viverra ullamcorper laoreet. Integer pharetra nec dolor id ornare. Mauris sit amet tincidunt lectus.",
-    approvers: [
-      {
-        name: "Alberto Luis Linao",
-        status: "approved",
-      },
-    ],
-  },
-  {
-    id: 4,
-    ref: "IT-4805",
-    formType: "it",
-    requestTitle: "IT 4805",
-    status: "cancelled",
-    lastUpdated: "11/15/21",
-    requestedBy: "Alvin Ramos",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sagittis odio ut bibendum bibendum. Nullam faucibus ex vitae varius molestie. Pellentesque tincidunt sit amet mauris eu malesuada. Etiam auctor ex sed mattis imperdiet. Nulla viverra ullamcorper laoreet. Integer pharetra nec dolor id ornare. Mauris sit amet tincidunt lectus.",
-    approvers: [
-      {
-        name: "Lance Juat",
-        status: "pending",
-      },
-    ],
-  },
-  {
-    id: 5,
-    ref: "IT-4830",
-    formType: "it",
-    requestTitle: "IT 4830",
-    status: "pending",
-    lastUpdated: "11/16/21",
-    requestedBy: "Rico Precioso",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sagittis odio ut bibendum bibendum. Nullam faucibus ex vitae varius molestie. Pellentesque tincidunt sit amet mauris eu malesuada. Etiam auctor ex sed mattis imperdiet. Nulla viverra ullamcorper laoreet. Integer pharetra nec dolor id ornare. Mauris sit amet tincidunt lectus.",
-    approvers: [
-      {
-        name: "Lance Juat",
-        status: "pending",
-      },
-    ],
-  },
-  {
-    id: 6,
-    ref: "PTRF-5644",
-    formType: "it",
-    requestTitle: "From HR to IT",
-    status: "forRevision",
-    lastUpdated: "09/29/22",
-    requestedBy: "Christine Molina",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sagittis odio ut bibendum bibendum. Nullam faucibus ex vitae varius molestie. Pellentesque tincidunt sit amet mauris eu malesuada. Etiam auctor ex sed mattis imperdiet. Nulla viverra ullamcorper laoreet. Integer pharetra nec dolor id ornare. Mauris sit amet tincidunt lectus.",
-    approvers: [
-      {
-        name: "Alberto Luis Linao",
-        status: "forRevision",
-      },
-    ],
-  },
-  {
-    id: 7,
-    ref: "PTRF-5645",
-    formType: "it",
-    requestTitle: "ILIJAN LNG - PTRF",
-    status: "stale",
-    lastUpdated: "09/29/22",
-    requestedBy: "Arianne Roxas",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sagittis odio ut bibendum bibendum. Nullam faucibus ex vitae varius molestie. Pellentesque tincidunt sit amet mauris eu malesuada. Etiam auctor ex sed mattis imperdiet. Nulla viverra ullamcorper laoreet. Integer pharetra nec dolor id ornare. Mauris sit amet tincidunt lectus.",
-    approvers: [
-      {
-        name: "Maria Deanna Romero",
-        status: "pending",
-      },
-    ],
-  },
-  {
-    id: 8,
-    ref: "RF-53",
-    formType: "requisitionForm",
-    requestTitle: "Requisition Form 53",
-    status: "approved",
-    lastUpdated: "11/16/22",
-    requestedBy: "Arianne Roxas",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sagittis odio ut bibendum bibendum. Nullam faucibus ex vitae varius molestie. Pellentesque tincidunt sit amet mauris eu malesuada. Etiam auctor ex sed mattis imperdiet. Nulla viverra ullamcorper laoreet. Integer pharetra nec dolor id ornare. Mauris sit amet tincidunt lectus.",
-    approvers: [
-      {
-        name: "Alberto Luis Linao",
-        status: "approved",
-      },
-      {
-        name: "Maria Deanna Romero",
-        status: "approved",
-      },
-    ],
-  },
-  {
-    id: 9,
-    ref: "RF-408",
-    formType: "requisitionForm",
-    requestTitle: "Requisition Form 408",
-    status: "pending",
-    lastUpdated: "12/02/20",
-    requestedBy: "Arianne Roxas",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sagittis odio ut bibendum bibendum. Nullam faucibus ex vitae varius molestie. Pellentesque tincidunt sit amet mauris eu malesuada. Etiam auctor ex sed mattis imperdiet. Nulla viverra ullamcorper laoreet. Integer pharetra nec dolor id ornare. Mauris sit amet tincidunt lectus.",
-    approvers: [
-      {
-        name: "Lance Juat",
-        status: "pending",
-      },
-      {
-        name: "Maria Deanna Romero",
-        status: "pending",
-      },
-    ],
-  },
-  {
-    id: 10,
-    ref: "RF-409",
-    formType: "requisitionForm",
-    requestTitle: "Requisition Form 409",
-    status: "pending",
-    lastUpdated: "12/02/20",
-    requestedBy: "Arianne Roxas",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sagittis odio ut bibendum bibendum. Nullam faucibus ex vitae varius molestie. Pellentesque tincidunt sit amet mauris eu malesuada. Etiam auctor ex sed mattis imperdiet. Nulla viverra ullamcorper laoreet. Integer pharetra nec dolor id ornare. Mauris sit amet tincidunt lectus.",
-    approvers: [
-      {
-        name: "Alberto Luis Linao",
-        status: "pending",
-      },
-      {
-        name: "Lance Juat",
-        status: "approved",
-      },
-    ],
-  },
-];
-
 type Props = {
-  formType: string | null;
-  status: string | null;
-  search: string;
+  visibleRequests: RequestType[];
+  selectedRequest: RequestType | null;
+  setSelectedRequest: Dispatch<SetStateAction<RequestType | null>>;
+  isApprover: boolean;
+  handleApprove: () => void;
+  handleSendToRevision: () => void;
+  handleReject: () => void;
 };
 
-const REQUEST_PER_PAGE = 8;
-
-const RequestTable = ({ formType, status, search }: Props) => {
+const RequestTable = ({
+  visibleRequests,
+  selectedRequest,
+  setSelectedRequest,
+  isApprover,
+  handleApprove,
+  handleSendToRevision,
+  handleReject,
+}: Props) => {
   const { width } = useViewportSize();
   const router = useRouter();
-
-  const [activePage, setPage] = useState(1);
-  const [requestList, setRequestList] = useState<RequestType[]>(tempRequests);
-  const [visibleRequests, setVisibleRequests] = useState(
-    tempRequests.slice(0, REQUEST_PER_PAGE)
-  );
-  const [selectedRequest, setSelectedRequest] = useState<RequestType | null>(
-    null
-  );
-  const [isApprover, setIsApprover] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    let newRequestList: RequestType[] = tempRequests;
-    if (formType) {
-      newRequestList = newRequestList.filter(
-        (request) => request.formType === formType
-      );
-    }
-    if (status) {
-      newRequestList = newRequestList.filter(
-        (request) => request.status === status
-      );
-    }
-    if (search) {
-      newRequestList = newRequestList.filter((request) =>
-        toLower(request.requestTitle).includes(toLower(search))
-      );
-    }
-
-    console.log;
-    setRequestList(newRequestList);
-    filterPage(1, newRequestList);
-  }, [search, formType, status]);
-
-  useEffect(() => {
-    setIsApprover(false);
-    if (selectedRequest) {
-      if (
-        selectedRequest.status === "stale" ||
-        selectedRequest.status === "pending"
-      ) {
-        selectedRequest.approvers.map((approver) => {
-          if (approver.name === currentUser && approver.status === "pending") {
-            setIsApprover(true);
-          }
-        });
-      }
-    }
-  }, [selectedRequest]);
-
-  const filterPage = (page: number, requestList: RequestType[]) => {
-    setSelectedRequest(null);
-    setPage(page);
-    setVisibleRequests(
-      requestList.slice(
-        (page - 1) * REQUEST_PER_PAGE,
-        (page - 1) * REQUEST_PER_PAGE + REQUEST_PER_PAGE
-      )
-    );
-  };
 
   const rows = visibleRequests.map((request) => (
     <tr
@@ -348,43 +103,8 @@ const RequestTable = ({ formType, status, search }: Props) => {
     </tr>
   ));
 
-  const handleApprove = () => {
-    setIsLoading(true);
-    showNotification({
-      title: "Success!",
-      message: `You approved ${selectedRequest?.requestTitle}`,
-      color: "green",
-    });
-    setSelectedRequest(null);
-    setIsLoading(false);
-  };
-
-  const handleSendToRevision = () => {
-    setIsLoading(true);
-    showNotification({
-      title: "Success!",
-      message: `${selectedRequest?.requestTitle} is Sent to Revision`,
-      color: "green",
-    });
-    setSelectedRequest(null);
-    setIsLoading(false);
-  };
-
-  const handleReject = () => {
-    setIsLoading(true);
-    showNotification({
-      title: "Success!",
-      message: `You rejected ${selectedRequest?.requestTitle}`,
-      color: "green",
-    });
-    setSelectedRequest(null);
-    setIsLoading(false);
-  };
-
   return (
     <>
-      <LoadingOverlay visible={isLoading} overlayBlur={2} />
-
       <Group align="flex-start">
         <Table
           mt="xl"
@@ -478,14 +198,6 @@ const RequestTable = ({ formType, status, search }: Props) => {
           </Paper>
         ) : null}
       </Group>
-      {requestList.length / REQUEST_PER_PAGE > 1 ? (
-        <Pagination
-          className={styles.pagination}
-          page={activePage}
-          onChange={(page) => filterPage(page, requestList)}
-          total={ceil(requestList.length / REQUEST_PER_PAGE)}
-        />
-      ) : null}
     </>
   );
 };
