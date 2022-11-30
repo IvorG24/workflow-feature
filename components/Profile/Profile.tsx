08f10465919b539dbadd02db0121ebab58d0611a
Renzo Visperas <renzovisperas07@gmail.com>
2022-11-30 14:38:28 +0800

    create team.json

--- components/Profile/Profile.tsx
+++ components/Profile/Profile.tsx
@@ -20,7 +20,7 @@ import {
 import { useRouter } from "next/router";
 import { useState } from "react";
 import { MEMBERS } from "../../tempData";
-import Assesment from "./Assessment";
+import AssessmentPage from "../AssessmentPage/AssessmentPage";
 import Bio from "./Bio";
 import EditProfileForm from "./EditProfileForm";
 import styles from "./Profile.module.scss";
@@ -113,7 +113,7 @@ const Profile = () => {
         </Tabs.Panel>
 
         <Tabs.Panel value="assessment" pt="xl">
-          <Assesment user={user} />
+          <AssessmentPage user={user} />
         </Tabs.Panel>
       </Tabs>
       <Modal
