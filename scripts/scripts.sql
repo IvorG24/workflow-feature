7f236bec5bb69e2e0be70754468a6f792763cae0
jason-evangelista <evangelistajasonglenn@gmail.com>
2022-12-05 14:26:20 +0800

    Update script.sql

--- scripts/scripts.sql
+++ scripts/scripts.sql
@@ -80,6 +80,7 @@ CREATE TABLE form_table(
   form_id INT GENERATED ALWAYS AS IDENTITY UNIQUE PRIMARY KEY,
   form_name_id INT REFERENCES form_name_table(form_name_id),
   form_owner UUID REFERENCES auth.users(id),
+  form_type form_type,
   created_at TIMESTAMPTZ DEFAULT NOW(),
   question_id INT REFERENCES question_table(question_id),
   response_value VARCHAR(254)[],
