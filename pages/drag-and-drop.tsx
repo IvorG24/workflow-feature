0a30abad521cc7069a2bee31f24b700f188c4721
Albert Linao <5449869+alberto-linao@users.noreply.github.com>
2022-12-07 09:39:38 +0800

    Separate page, drag-and-drop component

--- pages/drag-and-drop.tsx
+++ pages/drag-and-drop.tsx
@@ -1,23 +1,5 @@
-import {
-  Badge,
-  Button,
-  Card,
-  Container,
-  Group,
-  Image,
-  Text,
-} from "@mantine/core";
-import { useEffect, useState } from "react";
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
-
-type ItemData = {
-  id: string;
-  src: string;
-  alt: string;
-  text: string;
-  badge: string;
-  buttonMessage: string;
-};
+import ItemList from "@/components/DragAndDrop/DragAndDrop";
+import { Container } from "@mantine/core";
 
 const mockData = [
   {
@@ -64,84 +46,10 @@ const mockData = [
 
 const savedOrder = ["a", "b", "c", "d", "e"];
 
-function Item({ id, src, alt, text, badge, buttonMessage }: ItemData) {
-  return (
-    <Card shadow="sm" p="lg" radius="md" withBorder>
-      <Card.Section>
-        <Image src={src} height={160} alt={alt} />
-      </Card.Section>
-
-      <Group position="apart" mt="md" mb="xs">
-        <Text weight={500}>
-          id:{id} - {text}
-        </Text>
-        <Badge color="pink" variant="light">
-          {badge}
-        </Badge>
-      </Group>
-
-      <Button variant="light" color="blue" fullWidth mt="md" radius="md">
-        {buttonMessage}
-      </Button>
-    </Card>
-  );
-}
-
 function DragAndDropPage(): JSX.Element {
-  const [winReady, setWinReady] = useState(false);
-  const [orderArr, setOrderArr] = useState(savedOrder);
-  const [cardData, setCardData] = useState(mockData);
-  useEffect(() => {
-    setWinReady(true);
-  }, []);
-  const handleOnDragEnd = (result) => {
-    // handles dragged to outside of draggable area
-    if (!result.destination) return;
-    // we use temp as staging for item
-    const tempArr = cardData;
-    const [tempItem] = tempArr.splice(result.source.index, 1);
-    tempArr.splice(result.destination.index, 0, tempItem);
-    setCardData(tempArr);
-    // take order of id's in modified tempArr
-    const arrOfIds = tempArr.map((item) => item.id);
-    setOrderArr(arrOfIds);
-    console.log(orderArr);
-    console.log(cardData);
-  };
   return (
     <Container>
-      <p>Hi</p>
-      {winReady && (
-        <DragDropContext onDragEnd={handleOnDragEnd}>
-          <Droppable droppableId="items">
-            {(provided) => {
-              return (
-                <ol {...provided.droppableProps} ref={provided.innerRef}>
-                  {cardData.map((item, idx) => {
-                    return (
-                      <Draggable
-                        key={item.id}
-                        draggableId={item.id}
-                        index={idx}
-                      >
-                        {(provided) => (
-                          <li
-                            {...provided.draggableProps}
-                            {...provided.dragHandleProps}
-                            ref={provided.innerRef}
-                          >
-                            <Item {...item} />
-                          </li>
-                        )}
-                      </Draggable>
-                    );
-                  })}
-                </ol>
-              );
-            }}
-          </Droppable>
-        </DragDropContext>
-      )}
+      <ItemList data={mockData} order={savedOrder} />
     </Container>
   );
 }
