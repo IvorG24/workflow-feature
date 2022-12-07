c736e38c0d5509c83a364d25e9752ecf3c53ce02
Juan Carlos Lumingkit <juancarloslumingkit@gmail.com>
2022-12-08 00:15:19 +0800

    add update member roles functionality

--- components/MembersPage/MembersPage.tsx
+++ components/MembersPage/MembersPage.tsx
@@ -1,11 +1,13 @@
 // todo: create unit tests
 // todo: improve mobile responsiveness and improve layout
+import supabase from "@/utils/supabase";
 import { UserProfile } from "@/utils/types";
 import { Divider, Grid, Stack, Text, Title } from "@mantine/core";
-import { useSessionContext } from "@supabase/auth-helpers-react";
+import { showNotification } from "@mantine/notifications";
+import { useSessionContext, useUser } from "@supabase/auth-helpers-react";
 import { lowerCase } from "lodash";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import InviteTeamMembersSection from "./InviteTeamMembersSection";
 import MembersTable from "./MembersTable";
 import SearchBar from "./SearchBar";
@@ -18,57 +20,98 @@ export type TeamMember = {
 };
 
 const Member = () => {
-  const [searchBarValue, setSearchBarValue] = useState("");
-  const { supabaseClient } = useSessionContext();
   const router = useRouter();
-  const [memberList, setMemberList] = useState<TeamMember[]>([]);
+  const user = useUser();
   const { tid } = router.query;
+  const { supabaseClient } = useSessionContext();
+  const [searchBarValue, setSearchBarValue] = useState("");
+  const [memberList, setMemberList] = useState<TeamMember[]>([]);
+  const [authUserRole, setAuthUserRole] = useState<string>("");
+  const rolesOrder = ["owner", "admin", "member"];
+  // sort A-Z and Owner > Admin > Member
+  const sortedMemberList = useMemo(
+    () => sortMemberList(memberList),
+    [memberList]
+  );
 
-  useEffect(() => {
-    (async () => {
-      try {
-        const { data: team_role_table, error } = await supabaseClient
-          .from("team_role_table")
-          .select(`team_id, team_role, lock_account, user_profile_table(*)`)
-          .eq("team_id", tid);
+  const handleUpdateMemberRole = async (
+    memberId: string,
+    memberRole: string,
+    newRole: string
+  ) => {
+    const authUserRoleIndex = rolesOrder.indexOf(authUserRole);
+    const newRoleIndex = rolesOrder.indexOf(newRole);
 
-        if (error) throw error;
+    if (authUserRoleIndex >= 1 || newRoleIndex === 0) {
+      return showNotification({
+        title: "Error!",
+        message: `You don't have permission to perform this action.`,
+        color: "red",
+      });
+    }
 
-        setMemberList(sortMemberList(team_role_table) as TeamMember[]);
-      } catch (error) {
-        console.error(error);
-      }
-    })();
-  }, [supabaseClient, tid, searchBarValue]);
+    if (newRole === memberRole) {
+      return showNotification({
+        title: "Update invalid.",
+        message: `User already has a ${memberRole} role.`,
+        color: "orange",
+      });
+    }
 
-  // sort A-Z and Owner > Admin > Manager/Member
-  const sortMemberList = (members: TeamMember[]) => {
-    members.sort((a, b) => {
-      const nameA = a.user_profile_table.full_name;
-      const nameB = b.user_profile_table.full_name;
-      if (nameA !== null && nameB !== null) {
-        return nameA < nameB ? -1 : 1;
-      }
-      return 0;
-    });
+    try {
+      const { error } = await supabase
+        .from("team_role_table")
+        .update({ team_role: newRole })
+        .eq("user_id", memberId);
 
-    // todo: update member roles to match team_role_table
-    const rolesOrder = ["owner", "admin", "manager", "member"];
+      if (error) throw error;
 
-    members.sort((a, b) => {
-      const indexA = rolesOrder.indexOf(a.team_role);
-      const indexB = rolesOrder.indexOf(b.team_role);
-      return indexA - indexB;
-    });
-    return members;
+      showNotification({
+        title: "Success",
+        message: `Member role updated.`,
+        color: "green",
+      });
+      fetchTeamMembers();
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  const filterMemberList = memberList.filter((member) => {
+  const filterMemberList = sortedMemberList.filter((member) => {
     const memberName = member.user_profile_table.full_name;
     if (memberName) {
       return lowerCase(memberName).includes(searchBarValue);
     }
   });
+
+  const fetchTeamMembers = useCallback(async () => {
+    try {
+      const { data: team_role_table, error } = await supabaseClient
+        .from("team_role_table")
+        .select(`team_id, team_role, lock_account, user_profile_table(*)`)
+        .eq("team_id", tid);
+
+      if (error) throw error;
+
+      setMemberList(team_role_table as TeamMember[]);
+    } catch (error) {
+      console.error(error);
+    }
+  }, [tid, supabaseClient]);
+
+  useEffect(() => {
+    fetchTeamMembers();
+  }, [fetchTeamMembers]);
+
+  useEffect(() => {
+    if (user) {
+      const userRole = memberList.find(
+        (member) => member.user_profile_table.user_id === user.id
+      )?.team_role;
+
+      userRole !== undefined && setAuthUserRole(userRole);
+    }
+  }, [user, memberList]);
 
   return (
     <Stack>
@@ -83,7 +126,12 @@ const Member = () => {
             value={searchBarValue}
             numberOfMembers={memberList.length}
           />
-          <MembersTable members={filterMemberList} />
+          <MembersTable
+            authUserRole={authUserRole}
+            authUser={user}
+            members={filterMemberList}
+            updateMemberRole={handleUpdateMemberRole}
+          />
         </Grid.Col>
       </Grid>
       <Divider my={{ base: 10, lg: 20 }} />
@@ -102,6 +150,27 @@ const Member = () => {
       </Grid>
     </Stack>
   );
+};
+
+const sortMemberList = (members: TeamMember[]) => {
+  members.sort((a, b) => {
+    const nameA = a.user_profile_table.full_name;
+    const nameB = b.user_profile_table.full_name;
+    if (nameA !== null && nameB !== null) {
+      return nameA < nameB ? -1 : 1;
+    }
+    return 0;
+  });
+
+  // todo: update member roles to match team_role
+  const rolesOrder = ["owner", "admin", "manager", "member"];
+
+  members.sort((a, b) => {
+    const indexA = rolesOrder.indexOf(a.team_role);
+    const indexB = rolesOrder.indexOf(b.team_role);
+    return indexA - indexB;
+  });
+  return members;
 };
 
 export default Member;
--- components/MembersPage/MembersTable.tsx
+++ components/MembersPage/MembersTable.tsx
@@ -14,17 +14,31 @@ import {
 import { TeamMember } from "./MembersPage";
 
 import SvgArrowDropDown from "@/components/Icon/ArrowDropDown";
+import { User } from "@supabase/supabase-js";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import SvgMoreHoriz from "../Icon/MoreHoriz";
 
 type Props = {
   members: TeamMember[];
+  authUser: User | null;
+  authUserRole: string;
+  updateMemberRole: (
+    memberId: string,
+    memberRole: string,
+    newRole: string
+  ) => void;
 };
 
-const MembersTable = ({ members }: Props) => {
+const MembersTable = ({
+  members,
+  authUser,
+  authUserRole,
+  updateMemberRole,
+}: Props) => {
   const router = useRouter();
   const { tid } = router.query;
+  // pagination
   const [pageNumber, setPageNumber] = useState(1);
   const pageSize = 7;
   const totalPages = Math.ceil(members.length / pageSize);
@@ -38,64 +52,72 @@ const MembersTable = ({ members }: Props) => {
   };
 
   return (
-    <Stack justify="space-between" mih="300px" pt="md">
-      {
-        // todo: update properties to match fetched data
-        paginate(members, pageSize, pageNumber).map((member) => {
-          return (
-            <Grid
-              key={member.user_profile_table.user_id}
-              sx={{ borderTop: "1px solid #E9E9E9" }}
-            >
-              <Grid.Col order={1} span={10} sm={8}>
-                <Group
-                  onClick={() =>
-                    router.push(
-                      `/t/${tid}/profiles/${member.user_profile_table.user_id}/bio`
-                    )
-                  }
-                >
-                  <MantineAvatar
-                    size={40}
-                    radius={40}
-                    src={member.user_profile_table.avatar_url}
-                    alt={`${member.user_profile_table.full_name}'s Formsly Avatar`}
-                  />
-                  <Box>
-                    <Text fw="bold" color="dark">
-                      {member.user_profile_table.full_name}
-                    </Text>
-                    <Text fz="xs">{member.user_profile_table.email}</Text>
-                  </Box>
-                </Group>
-              </Grid.Col>
-              <Grid.Col order={3} orderXs={2} span={1} offset={2} offsetXs={1}>
-                <Select
-                  value={member.team_role}
-                  onChange={(e) => console.log(e)}
-                  data={[
-                    { value: "admin", label: "Admin" },
-                    { value: "manager", label: "Manager" },
-                    { value: "member", label: "Member" },
-                    { value: "owner", label: "Owner" },
-                  ]}
-                  radius={4}
-                  size="xs"
-                  w="90px"
-                  rightSection={<SvgArrowDropDown />}
-                  rightSectionWidth={20}
-                  readOnly
+    <Stack mih="300px" pt="md">
+      {paginate(members, pageSize, pageNumber).map((member) => {
+        return (
+          <Grid
+            key={member.user_profile_table.user_id}
+            sx={{ borderTop: "1px solid #E9E9E9" }}
+          >
+            <Grid.Col order={1} span={10} sm={8}>
+              <Group
+                onClick={() =>
+                  router.push(
+                    `/t/${tid}/profiles/${member.user_profile_table.user_id}/bio`
+                  )
+                }
+              >
+                <MantineAvatar
+                  size={40}
+                  radius={40}
+                  src={member.user_profile_table.avatar_url}
+                  alt={`${member.user_profile_table.full_name}'s Formsly Avatar`}
                 />
-              </Grid.Col>
-              <Grid.Col order={2} orderXs={3} span={1} offsetXs={1}>
-                <Button variant="subtle" size="xs" color="dark" fz="xl">
-                  <SvgMoreHoriz />
-                </Button>
-              </Grid.Col>
-            </Grid>
-          );
-        })
-      }
+                <Box>
+                  <Text fw="bold" color="dark">
+                    {member.user_profile_table.full_name}
+                  </Text>
+                  <Text fz="xs">{member.user_profile_table.email}</Text>
+                </Box>
+              </Group>
+            </Grid.Col>
+            <Grid.Col order={3} orderXs={2} span={1} offset={2} offsetXs={1}>
+              <Select
+                value={member.team_role}
+                onChange={(newRole: string) =>
+                  updateMemberRole(
+                    member.user_profile_table.user_id,
+                    member.team_role,
+                    newRole
+                  )
+                }
+                data={[
+                  { value: "owner", label: "Owner" },
+                  { value: "admin", label: "Admin" },
+                  // { value: "manager", label: "Manager" },
+                  { value: "member", label: "Member" },
+                ]}
+                radius={4}
+                size="xs"
+                w="90px"
+                rightSection={<SvgArrowDropDown />}
+                rightSectionWidth={20}
+                readOnly={
+                  authUserRole === "member" ||
+                  authUser?.id === member.user_profile_table.user_id
+                    ? true
+                    : false
+                }
+              />
+            </Grid.Col>
+            <Grid.Col order={2} orderXs={3} span={1} offsetXs={1}>
+              <Button variant="subtle" size="xs" color="dark" fz="xl">
+                <SvgMoreHoriz />
+              </Button>
+            </Grid.Col>
+          </Grid>
+        );
+      })}
       <Flex
         justify={{ base: "center", md: "flex-end" }}
         sx={{ borderTop: "1px solid #E9E9E9" }}
