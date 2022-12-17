4901d9a480a933ddf462bb937d7baef5ea2f53eb
GGianfranco <freelancegianfranco@gmail.com>
2022-12-17 10:19:13 +0800

    Merge branch 'preview' into origin/lance/delete-request-functionality

--- pages/t/[tid]/requests/[id].tsx
+++ pages/t/[tid]/requests/[id].tsx
@@ -1,15 +1,35 @@
 import TeamLayout from "@/components/Layout/TeamLayout";
 import Meta from "@/components/Meta/Meta";
 import Request from "@/components/Request/Request";
+import { retrieveRequest } from "@/utils/queries";
+import { RequestType } from "@/utils/types";
+import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { GetServerSideProps } from "next";
 import { ReactElement } from "react";
-import type { NextPageWithLayout } from "../../../_app";
 
-const RequestPage: NextPageWithLayout = () => {
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const supabase = createServerSupabaseClient(ctx);
+
+  const selectedRequest = await retrieveRequest(
+    supabase,
+    Number(ctx.params?.id)
+  );
+
+  return {
+    props: { selectedRequest },
+  };
+};
+
+type Props = {
+  selectedRequest: RequestType;
+};
+
+const RequestPage = ({ selectedRequest }: Props) => {
   // todo: fix meta tags
   return (
     <div>
       <Meta description="Specific Request" url="localhost:3000/requests/id" />
-      <Request />
+      <Request view="full" selectedRequest={selectedRequest} />
     </div>
   );
 };
