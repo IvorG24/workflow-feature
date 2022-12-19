26c04f221101eb9e3af9d25de7fdcd0f67cbd63a
Juan Carlos Lumingkit <juancarloslumingkit@gmail.com>
2022-12-19 13:53:39 +0800

    remove unnecessary comment

--- components/Layout/TeamLayout.tsx
+++ components/Layout/TeamLayout.tsx
@@ -139,32 +139,6 @@ const TeamLayout = ({ children }: Props) => {
           }
           header={
             <Header openNavbar={openNavbar} setOpenNavbar={setOpenNavbar} />
-            // <MantineHeader height={{ base: 60 }} p="sm">
-            //   <Flex justify="space-between" align="center" h="100%" py="md">
-            //     <Group>
-            //       <Image
-            //         src={`/image/logo-${colorScheme}.png`}
-            //         alt="logo"
-            //         width={147}
-            //         height={52}
-            //       />
-            //       <ActionIcon
-            //         variant="default"
-            //         onClick={() => toggleColorScheme()}
-            //         aria-label="toggle dark mode"
-            //       >
-            //         {colorScheme === "dark" ? <SvgSun /> : <SvgMoon />}
-            //       </ActionIcon>
-            //     </Group>
-            //     <MediaQuery largerThan="sm" styles={{ display: "none" }}>
-            //       <Burger
-            //         opened={openNavbar}
-            //         onClick={() => setOpenNavbar((prev) => !prev)}
-            //         size="sm"
-            //       />
-            //     </MediaQuery>
-            //   </Flex>
-            // </MantineHeader>
           }
         >
           <Container p={0} fluid>
