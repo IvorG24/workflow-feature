f3c3d1e4b50200b43eeb9fb46235f42dad692b46
Juan Carlos Lumingkit <juancarloslumingkit@gmail.com>
2023-01-11 21:14:51 +0800

    add DownloadToPDF feature

---
+++ components/Request/PdfPreview.tsx
@@ -0,0 +1,74 @@
+import { GetTeamRequestList } from "@/utils/queries-new";
+import { setBadgeColor } from "@/utils/request";
+import { Box, Divider, Group, Text, Title } from "@mantine/core";
+import { startCase } from "lodash";
+
+type Props = {
+  request: GetTeamRequestList[0];
+  attachments: { filepath: string; url: string | null }[] | undefined;
+};
+
+const PdfPreview = ({ request, attachments }: Props) => {
+  return (
+    <Box
+      id={`${request.request_id}`}
+      p="md"
+      sx={{
+        border: "1px solid #e9e9e9",
+        backgroundColor: "#FFFFFF",
+      }}
+    >
+      {/* add team logo? */}
+      <Title align="center" order={4} mb="md">
+        {request.team_name}
+      </Title>
+
+      <Group position="apart">
+        <Text fz="md" fw={700} c="dark.9">
+          {request.form_name}
+        </Text>
+
+        <Text
+          fw={700}
+          color={setBadgeColor(request.request_status_id as string)}
+        >
+          {startCase(request.request_status_id as string)}
+        </Text>
+      </Group>
+      <Divider my="xs" />
+      <Group position="apart">
+        <Text fw={500} c="dark.9">
+          {request.request_title}
+        </Text>
+      </Group>
+      <Text color="dark.9">{request.request_date_created?.slice(0, 10)}</Text>
+      <Text my="sm" color="dark.9">
+        {request.request_description}
+      </Text>
+      <Group>
+        <Box>
+          <Text fw={500} c="dark.9">
+            Requested By
+          </Text>
+          <Text color="dark.9">{`${request.user_first_name} ${request.user_last_name}`}</Text>
+        </Box>
+        <Box>
+          <Text fw={500} c="dark.9">
+            Approved By
+          </Text>
+          <Text c="dark.9">N/A</Text>
+        </Box>
+      </Group>
+      <Box mt="md">
+        <Text fw={500} color="dark.9">
+          Attachments
+        </Text>
+        {attachments?.map((item, idx: number) => {
+          return <Text key={idx}>{item.filepath}</Text>;
+        })}
+      </Box>
+    </Box>
+  );
+};
+
+export default PdfPreview;
--- components/Request/RequestItem.tsx
+++ components/Request/RequestItem.tsx
@@ -10,15 +10,18 @@ import {
   CloseButton,
   Divider,
   Group,
+  Modal,
   SimpleGrid,
   Text,
   Title,
 } from "@mantine/core";
 import { showNotification } from "@mantine/notifications";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
-import { IconDotsVertical } from "@tabler/icons";
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { IconDotsVertical, IconDownload } from "@tabler/icons";
+import jsPDF from "jspdf";
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
 import AttachmentPill from "../RequestsPage/AttachmentPill";
+import PdfPreview from "./PdfPreview";
 import RequestComment from "./RequestComment";
 
 type Props = {
@@ -27,9 +30,10 @@ type Props = {
 };
 
 const RequestItem = ({ request, setSelectedRequest }: Props) => {
+  const requestRef = useRef<HTMLDivElement>(null);
+  const [openPdfPreview, setOpenPdfPreview] = useState(false);
   const [attachmentUrlList, setAttachmentUrlList] =
     useState<GetRequestWithAttachmentUrlList>();
-
   const supabaseClient = useSupabaseClient();
 
   const attachments = request.request_attachment_filepath_list?.map(
@@ -60,8 +64,48 @@ const RequestItem = ({ request, setSelectedRequest }: Props) => {
     };
   }, [request, supabaseClient]);
 
+  const handleDownloadToPdf = () => {
+    const html = document.getElementById(`${request.request_id}`);
+    const pdfHeight =
+      Number(`${html?.clientHeight}`) > 842
+        ? Number(`${html?.clientHeight}`) + 2
+        : 842;
+
+    const doc = new jsPDF({
+      orientation: "portrait",
+      unit: "px",
+      format: [pdfHeight, 592],
+    });
+
+    doc.html(html as HTMLElement, {
+      callback: (doc) => doc.save(`request_${request.request_title}`),
+      x: doc.internal.pageSize.width / 6,
+      y: 50,
+    });
+    setOpenPdfPreview(false);
+    return;
+  };
+
   return (
-    <Box>
+    <Box p="xs" ref={requestRef}>
+      {/* PDF PREVIEW */}
+      {request && (
+        <Modal
+          opened={openPdfPreview}
+          onClose={() => setOpenPdfPreview(false)}
+          title="Download Preview"
+        >
+          <PdfPreview request={request} attachments={attachments} />
+          <SimpleGrid cols={2} mt="xl">
+            <Button variant="default" onClick={() => setOpenPdfPreview(false)}>
+              Cancel
+            </Button>
+            <Button color="indigo" onClick={() => handleDownloadToPdf()}>
+              Download
+            </Button>
+          </SimpleGrid>
+        </Modal>
+      )}
       <Group position="apart">
         <Title order={4}>{request.request_title}</Title>
         <CloseButton
@@ -79,9 +123,14 @@ const RequestItem = ({ request, setSelectedRequest }: Props) => {
             </Text>
           </Box>
         </Group>
-        <Text fz="xs" c="dimmed">
-          <IconDotsVertical />
-        </Text>
+        <Group sx={{ cursor: "pointer" }}>
+          <Text fz="xs" c="dimmed" onClick={() => setOpenPdfPreview(true)}>
+            <IconDownload />
+          </Text>
+          <Text fz="xs" c="dimmed">
+            <IconDotsVertical />
+          </Text>
+        </Group>
       </Group>
       <Text>{request.request_description}</Text>
       <Divider my="sm" variant="dotted" />
