8a67ca86dc52c63a892fb5ce2206e8c1d96838a7
Juan Carlos Lumingkit <juancarloslumingkit@gmail.com>
2023-05-17 17:56:27 +0800

    NextJS initial setup: add MantineUI and Prettier config

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { Center, Container, Title } from "@mantine/core";
+
+export default function Home() {
+  return (
+    <Container h="100vh">
+      <Center>
+        <Title>Welcome to Formsly.io</Title>
+      </Center>
+    </Container>
+  );
+}
