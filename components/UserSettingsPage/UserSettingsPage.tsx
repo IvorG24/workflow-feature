a7a7292d0c8fe0821db5f7a09628f4396a055d8d
Mark Jay Lunas <you@exagit config --global user.email markjay.lunas@gmail.com git config --global user.email you@exa>
2023-05-24 11:24:41 +0800

    Connect updateUser and uploadImage backend

--- components/UserSettingsPage/UserSettingsPage.tsx
+++ components/UserSettingsPage/UserSettingsPage.tsx
@@ -1,10 +1,18 @@
+import { uploadImage } from "@/backend/api/post";
+import { udpateUser } from "@/backend/api/update";
+import { Database } from "@/utils/database";
 import { UserWithSignatureType } from "@/utils/types";
-import { Container, LoadingOverlay, Title } from "@mantine/core";
+import { Container, Title } from "@mantine/core";
+import { notifications } from "@mantine/notifications";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { now } from "lodash";
+import { useState } from "react";
 import { FormProvider, useForm } from "react-hook-form";
 import ChangePassword from "./ChangePassword";
 import PersonalInfo from "./PersonalInfo";
 
 export type PersonalInfoForm = {
+  user_id: string;
   user_email: string;
   user_username: string;
   user_first_name: string;
@@ -25,54 +33,75 @@ type Props = {
 };
 
 const UserSettingsPage = ({ user }: Props) => {
+  const supabaseClient = createBrowserSupabaseClient<Database>();
+
+  const [avatarFile, setAvatarFile] = useState<File | null>(null);
+  const [isUpdatingPersonalInfo, setIsUpdatingPersonalInfo] = useState(false);
+
   const personalInfoFormMethods = useForm<PersonalInfoForm>({
     defaultValues: {
+      user_id: user.user_id,
       user_email: user.user_email,
       user_username: user.user_username,
       user_first_name: user.user_first_name,
       user_last_name: user.user_last_name,
+      user_phone_number: user.user_phone_number || "",
+      user_job_title: user.user_job_title || "",
+      user_avatar: user.user_avatar || "",
     },
   });
 
   const changePasswordFormMethods = useForm<ChangePasswordForm>();
 
   const handleSavePersonalInfo = async (data: PersonalInfoForm) => {
-    console.log(data);
+    try {
+      setIsUpdatingPersonalInfo(true);
+
+      let imageUrl = "";
+      if (avatarFile) {
+        imageUrl = await uploadImage(supabaseClient, {
+          id: data.user_id,
+          image: avatarFile,
+          bucket: "USER_AVATARS",
+        });
+      }
+
+      await udpateUser(supabaseClient, {
+        ...data,
+        user_avatar: imageUrl ? `${imageUrl}?date=${now()}` : data.user_avatar,
+      });
+
+      notifications.show({
+        title: "Success!",
+        message: "Personal info updated.",
+        color: "green",
+      });
+    } catch (e) {
+      notifications.show({
+        title: "Something went wrong",
+        message: "Please try again later",
+        color: "red",
+      });
+    } finally {
+      setIsUpdatingPersonalInfo(false);
+    }
   };
 
   const handleChangePassword = async (data: ChangePasswordForm) => {
     console.log(data);
   };
 
-  // const supabaseClient = createBrowserSupabaseClient<Database>();
-
-  // const handleTestUpdateUser = async () => {
-  //   try {
-  //     await udpateUser(supabaseClient, {
-  //       user_id: TEMP_USER_ID,
-  //       user_first_name: "Updated First Name",
-  //       user_last_name: "Updated Last Name",
-  //       user_username: "updatedUserName",
-  //       user_phone_number: "9856895689",
-  //       user_job_title: "Updated Job Title",
-  //     });
-  //   } catch (e) {
-  //     console.log(e);
-  //   }
-  // };
-
   return (
     <Container fluid>
-      <LoadingOverlay
-        visible={false}
-        overlayBlur={2}
-        transitionDuration={500}
-      />
-
-      <Title>User Settings Page</Title>
+      <Title order={2}>User Settings</Title>
 
       <FormProvider {...personalInfoFormMethods}>
-        <PersonalInfo onSavePersonalInfo={handleSavePersonalInfo} />
+        <PersonalInfo
+          onSavePersonalInfo={handleSavePersonalInfo}
+          avatarFile={avatarFile}
+          onAvatarFileChange={setAvatarFile}
+          isUpdatingPersonalInfo={isUpdatingPersonalInfo}
+        />
       </FormProvider>
 
       <FormProvider {...changePasswordFormMethods}>
