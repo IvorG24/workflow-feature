795e27ed5b725c99c318623c65ab1e2b681ccbe8
Mark Jay Lunas <you@exagit config --global user.email markjay.lunas@gmail.com git config --global user.email you@exa>
2023-05-26 14:51:09 +0800

    Add date tooltip

--- components/RequestFormListPage/FormCard.tsx
+++ components/RequestFormListPage/FormCard.tsx
@@ -96,7 +96,7 @@ const FormCard = ({ form, onDeleteForm, onHideForm }: Props) => {
         <Flex justify="space-between">
           <Group spacing={4}>
             {/* todo: change into creator label and avatar */}
-            <Tooltip label="John Doe">
+            <Tooltip label="John Doe" openDelay={400}>
               <Avatar
                 src={
                   "https://avatars.githubusercontent.com/u/10353856?s=460&u=88394dfd67727327c1f7670a1764dc38a8a24831&v=4"
@@ -105,9 +105,16 @@ const FormCard = ({ form, onDeleteForm, onHideForm }: Props) => {
                 size={18}
               ></Avatar>
             </Tooltip>
-            <Text size="xs" color="dimmed">
-              Created {moment(form.form_date_created).fromNow()}
-            </Text>
+            <Tooltip
+              label={moment(form.form_date_created).format(
+                "MMMM Do YYYY, h:mm:ss a"
+              )}
+              openDelay={400}
+            >
+              <Text size="xs" color="dimmed">
+                Created {moment(form.form_date_created).fromNow()}
+              </Text>
+            </Tooltip>
           </Group>
 
           <Menu shadow="md" width={200} position="bottom-end">
