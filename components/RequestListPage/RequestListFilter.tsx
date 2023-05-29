b170af28ccfba97fe83568de0c811efd2b648e5f
Juan Carlos Lumingkit <juancarloslumingkit@gmail.com>
2023-05-29 13:03:20 +0800

    update filter behavior: only update when unfocused

--- components/RequestListPage/RequestListFilter.tsx
+++ components/RequestListPage/RequestListFilter.tsx
@@ -1,12 +1,4 @@
-import { getRequestList } from "@/backend/api/get";
-import { useLoadingActions } from "@/stores/useLoadingStore";
-import { useActiveTeam } from "@/stores/useTeamStore";
-import { DEFAULT_REQUEST_LIST_LIMIT } from "@/utils/contant";
-import {
-  FormStatusType,
-  RequestType,
-  TeamMemberWithUserType,
-} from "@/utils/types";
+import { RequestType, TeamMemberWithUserType } from "@/utils/types";
 import {
   ActionIcon,
   Group,
@@ -14,44 +6,38 @@ import {
   TextInput,
   Tooltip,
 } from "@mantine/core";
-import { notifications } from "@mantine/notifications";
-import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useFocusWithin } from "@mantine/hooks";
 import {
   IconSearch,
   IconSortAscending,
   IconSortDescending,
 } from "@tabler/icons-react";
-import { Dispatch, SetStateAction, useState } from "react";
-import { Controller, useForm } from "react-hook-form";
+import { Controller, useFormContext } from "react-hook-form";
+import { FilterFormValues } from "./RequestListPage";
 
 type RequestListFilterProps = {
   requestList: RequestType[];
   teamMemberList: TeamMemberWithUserType[];
-  setVisibleRequestList: Dispatch<SetStateAction<RequestType[]>>;
-};
-
-type FilterFormValues = {
-  search: string;
-  requestorList: string[];
-  formList: string[];
-  status?: FormStatusType[];
+  handleFilterForms: () => void;
 };
 
 const RequestListFilter = ({
   requestList,
   teamMemberList,
-  setVisibleRequestList,
+  handleFilterForms,
 }: RequestListFilterProps) => {
-  const supabaseClient = useSupabaseClient();
-  const { setIsLoading } = useLoadingActions();
-  const activeTeam = useActiveTeam();
-  const [isAscendingSort, setIsAscendingSort] = useState(false);
   const inputFilterProps = {
-    w: 300,
+    w: { base: 200, sm: 300 },
     clearable: true,
+    clearSearchOnChange: true,
+    clearSearchOnBlur: true,
     searchable: true,
     nothingFound: "Nothing found",
   };
+
+  const { ref: requestorRef, focused: requestorRefFocused } = useFocusWithin();
+  const { ref: formRef, focused: formRefFocused } = useFocusWithin();
+  const { ref: statusRef, focused: statusRefFocused } = useFocusWithin();
 
   const memberList = teamMemberList.map((member) => ({
     value: member.team_member_id,
@@ -78,123 +64,108 @@ const RequestListFilter = ({
     { value: "CANCELED", label: "Canceled" },
   ];
 
-  const { register, handleSubmit, getValues, control } =
-    useForm<FilterFormValues>({
-      mode: "onChange",
-    });
-
-  const handleFilterForms = async (
-    { search, requestorList, formList, status }: FilterFormValues = getValues()
-  ) => {
-    try {
-      setIsLoading(true);
-      const params = {
-        teamId: activeTeam.team_id,
-        page: 1,
-        limit: DEFAULT_REQUEST_LIST_LIMIT,
-        requestor:
-          requestorList && requestorList.length > 0 ? requestorList : undefined,
-        form: formList && formList.length > 0 ? formList : undefined,
-        status: status && status.length > 0 ? status : undefined,
-        search: search,
-      };
-      const { data, count } = await getRequestList(supabaseClient, {
-        ...params,
-        sort: isAscendingSort ? "ascending" : "descending",
-      });
-      setVisibleRequestList(data as RequestType[]);
-      console.log(count);
-    } catch (e) {
-      notifications.show({
-        title: "Something went wrong",
-        message: "Please try again later",
-        color: "red",
-      });
-    } finally {
-      setIsLoading(false);
-    }
-  };
+  const { register, getValues, control, setValue } =
+    useFormContext<FilterFormValues>();
 
   return (
-    <form onSubmit={handleSubmit(handleFilterForms)}>
-      <Group>
-        <Tooltip label={isAscendingSort ? "Ascending" : "Descending"}>
-          <ActionIcon
-            onClick={() => setIsAscendingSort(!isAscendingSort)}
-            size={36}
-            color="dark.3"
-            variant="outline"
-          >
-            {isAscendingSort ? (
-              <IconSortAscending size={18} />
-            ) : (
-              <IconSortDescending size={18} />
-            )}
+    <Group>
+      <Controller
+        control={control}
+        name="isAscendingSort"
+        defaultValue={true}
+        render={({ field: { value } }) => {
+          return (
+            <Tooltip
+              label={getValues("isAscendingSort") ? "Ascending" : "Descending"}
+              openDelay={800}
+            >
+              <ActionIcon
+                onClick={async () => {
+                  setValue("isAscendingSort", !getValues("isAscendingSort"));
+                  handleFilterForms();
+                }}
+                size={36}
+                color="dark.3"
+                variant="outline"
+              >
+                {value ? (
+                  <IconSortAscending size={18} />
+                ) : (
+                  <IconSortDescending size={18} />
+                )}
+              </ActionIcon>
+            </Tooltip>
+          );
+        }}
+      />
+      <TextInput
+        placeholder="Search by request id"
+        rightSection={
+          <ActionIcon size="xs" type="submit">
+            <IconSearch />
           </ActionIcon>
-        </Tooltip>
-        <TextInput
-          placeholder="Search by request id"
-          rightSection={
-            <ActionIcon size="xs" type="submit">
-              <IconSearch />
-            </ActionIcon>
-          }
-          w={300}
-          {...register("search")}
-        />
-        <Controller
-          control={control}
-          name="formList"
-          render={({ field: { value, onChange } }) => (
-            <MultiSelect
-              data={formList}
-              placeholder="Form"
-              value={value}
-              onChange={async (value) => {
-                onChange(value);
-                await handleFilterForms();
-              }}
-              {...inputFilterProps}
-            />
-          )}
-        />
+        }
+        w={{ base: 200, sm: 300 }}
+        {...register("search")}
+      />
+      <Controller
+        control={control}
+        name="formList"
+        render={({ field: { value, onChange } }) => (
+          <MultiSelect
+            data={formList}
+            placeholder="Form"
+            ref={formRef}
+            value={value}
+            onChange={(value) => {
+              onChange(value);
+              if (!formRefFocused) handleFilterForms();
+            }}
+            onDropdownClose={() => handleFilterForms()}
+            {...inputFilterProps}
+          />
+        )}
+      />
 
-        <Controller
-          control={control}
-          name="requestorList"
-          render={({ field: { value, onChange } }) => (
-            <MultiSelect
-              data={memberList}
-              placeholder="Requestor"
-              value={value}
-              onChange={async (value) => {
-                onChange(value);
-                await handleFilterForms();
-              }}
-              {...inputFilterProps}
-            />
-          )}
-        />
+      <Controller
+        control={control}
+        name="requestorList"
+        render={({ field: { value, onChange } }) => (
+          <MultiSelect
+            placeholder="Requestor"
+            ref={requestorRef}
+            data={memberList}
+            value={value}
+            onChange={(value) => {
+              onChange(value);
+              if (!requestorRefFocused) handleFilterForms();
+            }}
+            onDropdownClose={() => handleFilterForms()}
+            {...inputFilterProps}
+          />
+        )}
+      />
 
-        <Controller
-          control={control}
-          name="status"
-          render={({ field: { value, onChange } }) => (
-            <MultiSelect
-              data={statusList}
-              placeholder="Status"
-              value={value}
-              onChange={async (value) => {
-                onChange(value);
-                console.log(value);
-                await handleFilterForms();
-              }}
-              {...inputFilterProps}
-            />
-          )}
-        />
-      </Group>
-    </form>
+      <Controller
+        control={control}
+        name="status"
+        render={({ field: { value, onChange } }) => (
+          <MultiSelect
+            data={statusList}
+            placeholder="Status"
+            value={value}
+            onChange={async (value) => {
+              onChange(value);
+              console.log(!statusRefFocused && !value);
+              if (!statusRefFocused) handleFilterForms();
+            }}
+            ref={statusRef}
+            onDropdownClose={() => handleFilterForms()}
+            {...inputFilterProps}
+          />
+        )}
+      />
+    </Group>
   );
 };
 
--- components/RequestListPage/RequestListPage.tsx
+++ components/RequestListPage/RequestListPage.tsx
@@ -1,15 +1,26 @@
-import { RequestType, TeamMemberWithUserType } from "@/utils/types";
+import { getRequestList } from "@/backend/api/get";
+import { useActiveTeam } from "@/stores/useTeamStore";
+import { DEFAULT_REQUEST_LIST_LIMIT } from "@/utils/contant";
+import {
+  FormStatusType,
+  RequestType,
+  TeamMemberWithUserType,
+} from "@/utils/types";
 import {
   Box,
   Container,
   Flex,
   Group,
+  LoadingOverlay,
   Pagination,
   Space,
   Text,
   Title,
 } from "@mantine/core";
+import { notifications } from "@mantine/notifications";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
 import RequestCard from "./RequestCard/RequestCard";
 import RequestListFilter from "./RequestListFilter";
 
@@ -19,19 +30,68 @@ type Props = {
   teamMemberList: TeamMemberWithUserType[];
 };
 
-const RequestListPage = ({ requestList, teamMemberList }: Props) => {
+export type FilterFormValues = {
+  search: string;
+  requestorList: string[];
+  formList: string[];
+  status?: FormStatusType[];
+  isAscendingSort: boolean;
+};
+
+const RequestListPage = ({
+  requestList,
+  requestListCount,
+  teamMemberList,
+}: Props) => {
   const [visibleRequestList, setVisibleRequestList] = useState(requestList);
+  const [isFetchingRequestList, setIsFetchingRequestList] = useState(false);
   const [activePage, setActivePage] = useState(1);
-  const totalPages = Math.ceil(requestList.length / 12);
+  const supabaseClient = useSupabaseClient();
+  const activeTeam = useActiveTeam();
 
-  const handlePagination = () => {
-    const itemsPerPage = 12;
-    const startIndex = (activePage - 1) * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
-    const paginatedData = visibleRequestList.slice(startIndex, endIndex);
+  const filterFormMethods = useForm<FilterFormValues>({
+    defaultValues: { isAscendingSort: false },
+    mode: "onChange",
+  });
 
-    setVisibleRequestList(paginatedData);
-    setActivePage((prev) => prev + 1);
+  const { handleSubmit, getValues } = filterFormMethods;
+
+  const handleFilterForms = async (
+    {
+      search,
+      requestorList,
+      formList,
+      status,
+      isAscendingSort,
+    }: FilterFormValues = getValues()
+  ) => {
+    try {
+      setIsFetchingRequestList(true);
+      const params = {
+        teamId: activeTeam.team_id,
+        page: 1,
+        limit: DEFAULT_REQUEST_LIST_LIMIT,
+        requestor:
+          requestorList && requestorList.length > 0 ? requestorList : undefined,
+        form: formList && formList.length > 0 ? formList : undefined,
+        status: status && status.length > 0 ? status : undefined,
+        search: search,
+      };
+      const { data, count } = await getRequestList(supabaseClient, {
+        ...params,
+        sort: isAscendingSort ? "ascending" : "descending",
+      });
+      setVisibleRequestList(data as RequestType[]);
+      console.log(count);
+    } catch (e) {
+      notifications.show({
+        title: "Something went wrong",
+        message: "Please try again later",
+        color: "red",
+      });
+    } finally {
+      setIsFetchingRequestList(false);
+    }
   };
 
   return (
@@ -41,11 +101,15 @@ const RequestListPage = ({ requestList, teamMemberList }: Props) => {
         <Text>Manage your team requests here.</Text>
       </Box>
       <Group spacing={4}>
-        <RequestListFilter
-          requestList={requestList}
-          teamMemberList={teamMemberList}
-          setVisibleRequestList={setVisibleRequestList}
-        />
+        <FormProvider {...filterFormMethods}>
+          <form onSubmit={handleSubmit(handleFilterForms)}>
+            <RequestListFilter
+              requestList={requestList}
+              teamMemberList={teamMemberList}
+              handleFilterForms={handleFilterForms}
+            />
+          </form>
+        </FormProvider>
       </Group>
       <Space h="sm" />
       {visibleRequestList.length > 0 ? (
@@ -55,7 +119,9 @@ const RequestListPage = ({ requestList, teamMemberList }: Props) => {
           columnGap="md"
           rowGap="md"
           wrap="wrap"
+          pos="relative"
         >
+          <LoadingOverlay visible={isFetchingRequestList} overlayBlur={2} />
           {visibleRequestList.map((request) => (
             <RequestCard key={request.request_id} request={request} />
           ))}
@@ -67,8 +133,11 @@ const RequestListPage = ({ requestList, teamMemberList }: Props) => {
       )}
       <Pagination
         value={activePage}
-        onChange={handlePagination}
-        total={totalPages}
+        onChange={async (value) => {
+          setActivePage(value);
+          await handleFilterForms();
+        }}
+        total={Math.ceil(requestListCount / DEFAULT_REQUEST_LIST_LIMIT)}
         mt="xl"
         position="right"
       />
