250b0a957b1417dd3a112e23626b07ce10bdfebd
Juan Carlos Lumingkit <juancarloslumingkit@gmail.com>
2023-06-02 14:33:50 +0800

    add user order chart, team order chart, item purchase trend

--- components/RequisitionAnalyticsPage/LineChart.tsx
+++ components/RequisitionAnalyticsPage/LineChart.tsx
@@ -10,7 +10,7 @@ type Props = {
   data: DataItem[];
 };
 
-const BarChart: React.FC<Props> = ({ data }) => {
+const LineChart: React.FC<Props> = ({ data }) => {
   const chartRef = useRef<SVGSVGElement | null>(null);
 
   useEffect(() => {
@@ -18,29 +18,26 @@ const BarChart: React.FC<Props> = ({ data }) => {
       const svg = d3.select(chartRef.current);
       const width = 500;
       const height = 300;
-      const margin = { top: 20, right: 20, bottom: 30, left: 120 };
+      const margin = { top: 20, right: 20, bottom: 30, left: 40 };
       const innerWidth = width - margin.left - margin.right;
       const innerHeight = height - margin.top - margin.bottom;
 
-      const y = d3
+      const x = d3
         .scaleBand<string>()
         .domain(data.map((d) => d.label))
-        .range([0, innerHeight])
+        .range([0, innerWidth])
         .padding(0.1);
 
-      const x = d3
+      const y = d3
         .scaleLinear<number>()
         .domain([0, d3.max(data, (d) => d.value) || 0])
         .nice()
-        .range([0, innerWidth]);
+        .range([innerHeight, 0]);
 
-      const yAxis = (g: d3.Selection<SVGGElement, unknown, null, undefined>) =>
-        g.attr("transform", `translate(0,0)`).call(d3.axisLeft(y));
-
-      const xAxis = (g: d3.Selection<SVGGElement, unknown, null, undefined>) =>
-        g
-          .attr("transform", `translate(0,${innerHeight})`)
-          .call(d3.axisBottom(x));
+      const line = d3
+        .line<DataItem>()
+        .x((d) => x(d.label)!)
+        .y((d) => y(d.value));
 
       svg.attr("width", width).attr("height", height);
 
@@ -49,15 +46,29 @@ const BarChart: React.FC<Props> = ({ data }) => {
         .attr("transform", `translate(${margin.left},${margin.top})`);
 
       chartGroup
-        .append("g")
-        .attr("fill", "steelblue")
-        .selectAll("rect")
+        .append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 2)
+        .attr("d", line);
+
+      chartGroup
+        .selectAll("circle")
         .data(data)
-        .join("rect")
-        .attr("x", 0)
-        .attr("y", (d) => y(d.label)!)
-        .attr("width", (d) => x(d.value))
-        .attr("height", y.bandwidth());
+        .join("circle")
+        .attr("cx", (d) => x(d.label)!)
+        .attr("cy", (d) => y(d.value))
+        .attr("r", 4)
+        .attr("fill", "steelblue");
+
+      const xAxis = (g: d3.Selection<SVGGElement, unknown, null, undefined>) =>
+        g
+          .attr("transform", `translate(0,${innerHeight})`)
+          .call(d3.axisBottom(x));
+
+      const yAxis = (g: d3.Selection<SVGGElement, unknown, null, undefined>) =>
+        g.call(d3.axisLeft(y));
 
       chartGroup.append("g").call(xAxis);
       chartGroup.append("g").call(yAxis);
@@ -67,4 +78,4 @@ const BarChart: React.FC<Props> = ({ data }) => {
   return <svg ref={chartRef}></svg>;
 };
 
-export default BarChart;
+export default LineChart;
--- pages/team-requests/forms/[formId]/data.tsx
+++ pages/team-requests/forms/[formId]/data.tsx
@@ -1,14 +1,35 @@
+import { getForm } from "@/backend/api/get";
 import Meta from "@/components/Meta/Meta";
+import RequestFormPage from "@/components/RequestFormPage/RequestFormPage";
 import RequisitionAnalytics from "@/components/RequisitionAnalyticsPage/RequisitionAnalytics";
-import { TEMP_REQUISITION_FORM_RESPONSE } from "@/utils/dummyData";
+import {
+  TEMP_REQUISITION_FORM_PURCHASE_DATA,
+  TEMP_REQUISITION_FORM_TEAM_DATA,
+  TEMP_REQUISITION_FORM_USER_DATA,
+} from "@/utils/dummyData";
+import { FormWithResponseType } from "@/utils/types";
+import { Title } from "@mantine/core";
+import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { GetServerSideProps } from "next";
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
   try {
-    const rfData = TEMP_REQUISITION_FORM_RESPONSE;
+    const teamData = TEMP_REQUISITION_FORM_TEAM_DATA;
+    const userData = TEMP_REQUISITION_FORM_USER_DATA;
+    const purchaseData = TEMP_REQUISITION_FORM_PURCHASE_DATA;
+    const supabaseClient = createServerSupabaseClient(ctx);
+
+    const form = await getForm(supabaseClient, {
+      formId: `${ctx.query.formId}`,
+    });
 
     return {
-      props: { requisition_form_data: rfData },
+      props: {
+        requisition_form_team_data: teamData,
+        requisition_form_user_data: userData,
+        requisition_form_purchase_data: purchaseData,
+        form,
+      },
     };
   } catch (error) {
     console.error(error);
@@ -21,30 +42,51 @@ export const getServerSideProps: GetServerSideProps = async () => {
   }
 };
 
+export type RFDataType = {
+  request_response_id: string;
+  request_response: string;
+  request_response_request_id: string;
+  request_response_field_id: string;
+  request_response_date_purchased?: string;
+}[];
+
 type Props = {
-  requisition_form_data: {
-    request_response_id: string;
-    request_response: string;
-    request_response_request_id: string;
-    request_response_field_id: string;
-  }[];
+  requisition_form_team_data: RFDataType;
+  requisition_form_user_data: RFDataType;
+  requisition_form_purchase_data: RFDataType;
+  form: FormWithResponseType;
 };
 
-const RequisitionFormData = ({ requisition_form_data }: Props) => {
-  // const formslyForm = () => {
-  //   switch (form.form_name) {
-  //     case "Requisition Form":
-  //       return <>Formsly Req Form</>;
-  //   }
-  // };
+const RequisitionFormData = ({
+  requisition_form_team_data,
+  requisition_form_user_data,
+  requisition_form_purchase_data,
+  form,
+}: Props) => {
+  const formslyForm = () => {
+    switch (form.form_name) {
+      case "Requisition Form":
+        return (
+          <>
+            {" "}
+            <Title order={3}>{form.form_name}</Title>
+            <RequisitionAnalytics
+              teamRequisitionData={requisition_form_team_data}
+              userRequisitionData={requisition_form_user_data}
+              purchaseRequisitionData={requisition_form_purchase_data}
+            />
+          </>
+        );
+    }
+  };
   return (
     <>
       <Meta description="Request Page" url="/team-requests/forms/[formId]" />
-      {/* {form.form_is_formsly_form ? formslyForm() : null}
-      {!form.form_is_formsly_form ? <>Req form</> : null} */}
-      <RequisitionAnalytics requisitionData={requisition_form_data} />
+      {form.form_is_formsly_form ? formslyForm() : null}
+      {!form.form_is_formsly_form ? <RequestFormPage form={form} /> : null}
     </>
   );
 };
 
 export default RequisitionFormData;
+RequisitionFormData.Layout = "APP";
