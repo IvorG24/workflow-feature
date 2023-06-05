845c4c56d2c7d92954b230314b26c26bfee134ee
Juan Carlos Lumingkit <juancarloslumingkit@gmail.com>
2023-06-06 01:44:14 +0800

    add invitation card

--- components/TeamInvitationPage/TeamInvitaionPage.tsx
+++ components/TeamInvitationPage/TeamInvitaionPage.tsx
@@ -1,9 +1,36 @@
-import { Container, Title } from "@mantine/core";
+import {
+  Avatar,
+  Button,
+  Center,
+  Container,
+  Paper,
+  Stack,
+  Title,
+} from "@mantine/core";
 
 const TeamInvitationPage = () => {
   return (
-    <Container>
-      <Title>Team Invitation Page</Title>
+    <Container fluid>
+      <Center>
+        <Paper p="xl" mt="xl">
+          <Stack align="center">
+            <Avatar color="blue" size={120} radius={60}>
+              SC
+            </Avatar>
+            <Title align="center" weight={500} order={4}>
+              You have been invited to join Sta Clara Dev team.
+            </Title>
+            <Stack w="100%" mt="md" spacing="xs">
+              <Button fullWidth size="md">
+                Accept Invitation
+              </Button>
+              <Button fullWidth size="md" color="red">
+                Decline Invitation
+              </Button>
+            </Stack>
+          </Stack>
+        </Paper>
+      </Center>
     </Container>
   );
 };
--- pages/team/invitation/[invitationId].tsx
+++ pages/team/invitation/[invitationId].tsx
@@ -1,5 +1,47 @@
+import {
+  getTeam,
+  getUserActiveTeamId,
+  getUserTeamMemberId,
+} from "@/backend/api/get";
 import Meta from "@/components/Meta/Meta";
 import TeamInvitationPage from "@/components/TeamInvitationPage/TeamInvitaionPage";
+import { TEMP_USER_ID } from "@/utils/dummyData";
+import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { GetServerSideProps } from "next";
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  try {
+    const supabaseClient = createServerSupabaseClient(ctx);
+
+    const teamId = await getUserActiveTeamId(supabaseClient, {
+      userId: TEMP_USER_ID,
+    });
+
+    const team = await getTeam(supabaseClient, {
+      teamId: teamId,
+    });
+    if (!team) throw new Error("No team found");
+
+    const teamMemberId = await getUserTeamMemberId(supabaseClient, {
+      teamId: teamId,
+      userId: TEMP_USER_ID,
+    });
+
+    if (!teamMemberId) throw new Error("No team member found");
+
+    return {
+      props: { team, teamMemberId },
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      redirect: {
+        destination: "/500",
+        permanent: false,
+      },
+    };
+  }
+};
 
 const Page = () => {
   return (
