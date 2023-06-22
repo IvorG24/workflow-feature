9d8112b5afbc61d36011de9a0300cc071dc2a63e
Mark Jay Lunas <you@exagit config --global user.email markjay.lunas@gmail.com git config --global user.email you@exa>
2023-06-22 12:39:11 +0800

    Add confirm modal

--- components/TeamPage/TeamMemberMenu.tsx
+++ components/TeamPage/TeamMemberMenu.tsx
@@ -1,5 +1,6 @@
 import { MemberRoleType, TeamMemberType } from "@/utils/types";
-import { ActionIcon, Menu } from "@mantine/core";
+import { ActionIcon, Menu, Text } from "@mantine/core";
+import { openConfirmModal } from "@mantine/modals";
 import {
   IconArrowsLeftRight,
   IconDotsVertical,
@@ -8,6 +9,7 @@ import {
   IconUserShare,
   IconUserUp,
 } from "@tabler/icons-react";
+import { startCase } from "lodash";
 
 type Props = {
   member: TeamMemberType;
@@ -64,7 +66,26 @@ const TeamMemberMenu = ({
                   <Menu.Item
                     icon={<IconUserUp {...defaultMenuIconProps} />}
                     onClick={() =>
-                      onUpdateMemberRole(member.team_member_id, "ADMIN")
+                      openConfirmModal({
+                        title: <Text>Change Role</Text>,
+                        children: (
+                          <Text size={14}>
+                            Are you sure you want to promote
+                            <Text weight={700} span>
+                              &nbsp;
+                              {startCase(
+                                `${member.team_member_user.user_first_name} ${member.team_member_user.user_last_name}`
+                              )}
+                              &nbsp;
+                            </Text>
+                            to admin?
+                          </Text>
+                        ),
+                        labels: { confirm: "Confirm", cancel: "Cancel" },
+                        centered: true,
+                        onConfirm: () =>
+                          onUpdateMemberRole(member.team_member_id, "ADMIN"),
+                      })
                     }
                   >
                     Promote to Admin
@@ -73,7 +94,26 @@ const TeamMemberMenu = ({
                   <Menu.Item
                     icon={<IconUserDown {...defaultMenuIconProps} />}
                     onClick={() =>
-                      onUpdateMemberRole(member.team_member_id, "MEMBER")
+                      openConfirmModal({
+                        title: <Text>Change Role</Text>,
+                        children: (
+                          <Text size={14}>
+                            Are you sure you want to demote
+                            <Text weight={700} span>
+                              &nbsp;
+                              {startCase(
+                                `${member.team_member_user.user_first_name} ${member.team_member_user.user_last_name}`
+                              )}
+                              &nbsp;
+                            </Text>
+                            to member?
+                          </Text>
+                        ),
+                        labels: { confirm: "Confirm", cancel: "Cancel" },
+                        centered: true,
+                        onConfirm: () =>
+                          onUpdateMemberRole(member.team_member_id, "MEMBER"),
+                      })
                     }
                   >
                     Demote to Member
@@ -91,10 +131,31 @@ const TeamMemberMenu = ({
                   <Menu.Item
                     icon={<IconArrowsLeftRight {...defaultMenuIconProps} />}
                     onClick={() =>
-                      onTransferOwnership(
-                        authUser.team_member_id,
-                        member.team_member_id
-                      )
+                      openConfirmModal({
+                        title: <Text>Transfer Ownership</Text>,
+                        children: (
+                          <Text size={14}>
+                            Are you sure you want to transfer the ownership of
+                            this team to
+                            <Text weight={700} span>
+                              &nbsp;
+                              {startCase(
+                                `${member.team_member_user.user_first_name} ${member.team_member_user.user_last_name}`
+                              )}
+                            </Text>
+                            ?
+                          </Text>
+                        ),
+                        labels: { confirm: "Transfer", cancel: "Cancel" },
+                        centered: true,
+                        onConfirm: () =>
+                          onTransferOwnership(
+                            authUser.team_member_id,
+                            member.team_member_id
+                          ),
+
+                        confirmProps: { color: "red" },
+                      })
                     }
                   >
                     Transfer Team Ownership
@@ -104,7 +165,28 @@ const TeamMemberMenu = ({
                 <Menu.Item
                   color="red"
                   icon={<IconTrash {...defaultMenuIconProps} />}
-                  onClick={() => onRemoveFromTeam(member.team_member_id)}
+                  onClick={() =>
+                    openConfirmModal({
+                      title: <Text>Remove Team Member</Text>,
+                      children: (
+                        <Text size={14}>
+                          Are you sure you want to remove
+                          <Text weight={700} span>
+                            &nbsp;
+                            {startCase(
+                              `${member.team_member_user.user_first_name} ${member.team_member_user.user_last_name}`
+                            )}
+                            &nbsp;
+                          </Text>
+                          from this team?
+                        </Text>
+                      ),
+                      labels: { confirm: "Remove", cancel: "Cancel" },
+                      centered: true,
+                      onConfirm: () => onRemoveFromTeam(member.team_member_id),
+                      confirmProps: { color: "red" },
+                    })
+                  }
                 >
                   Remove From Team
                 </Menu.Item>
