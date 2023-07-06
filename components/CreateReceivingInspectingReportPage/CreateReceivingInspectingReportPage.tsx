8821ed2ac96167e467e7683df28c047e4c4c2058
RansuAndorei <lancejuat26@gmail.com>
2023-07-06 16:44:18 +0800

    add notification to request signers

--- components/CreateReceivingInspectingReportPage/CreateReceivingInspectingReportPage.tsx
+++ components/CreateReceivingInspectingReportPage/CreateReceivingInspectingReportPage.tsx
@@ -197,6 +197,9 @@ const CreateReceivingInspectingReportPage = ({ form, itemOptions }: Props) => {
           formId,
           teamMemberId: teamMember.team_member_id,
           signers: form.form_signer,
+          teamId: teamMember.team_member_team_id,
+          requesterName: `${requestorProfile.user_first_name} ${requestorProfile.user_last_name}`,
+          formName: form.form_name,
         });
 
         notifications.show({
