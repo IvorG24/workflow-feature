b21e5196fcc718d31076f23fb1378d80d43e5c2b
Juan Carlos Lumingkit <juancarloslumingkit@gmail.com>
2023-08-09 15:24:00 +0800

    add email invite notification

--- components/TeamPage/InviteMember.tsx
+++ components/TeamPage/InviteMember.tsx
@@ -1,8 +1,14 @@
+import { getTeamInvitation } from "@/backend/api/get";
+import { checkIfEmailExists, createTeamInvitation } from "@/backend/api/post";
+import { useActiveTeam } from "@/stores/useTeamStore";
+import { useUserTeamMember } from "@/stores/useUserStore";
 import {
+  Box,
   Button,
   Container,
   Divider,
   Flex,
+  Group,
   LoadingOverlay,
   MultiSelect,
   Paper,
@@ -10,28 +16,136 @@ import {
   Text,
 } from "@mantine/core";
 import { notifications } from "@mantine/notifications";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { IconMailPlus, IconUsersPlus } from "@tabler/icons-react";
-import { Dispatch, SetStateAction, useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import validator from "validator";
 
 type Props = {
-  isInvitingMember: boolean;
-  onInviteMember: () => void;
   memberEmailList: string[];
-  emailList: string[];
-  onSetEmailList: Dispatch<SetStateAction<string[]>>;
 };
 
-const InviteMember = ({
-  isInvitingMember,
-  onInviteMember,
-  memberEmailList,
-  emailList,
-  onSetEmailList,
-}: Props) => {
-  const [emailListData, setEmailListData] = useState<
-    { value: string; label: string }[]
-  >([]);
+type EmailListData = { value: string; label: string }[];
+
+type PendingInvite = {
+  invitation_id: string;
+  invitation_to_email: string;
+  invitation_date_created: string;
+  team_member: {
+    team_member_team_id: string;
+  };
+  is_resend_disabled?: boolean;
+};
+
+const InviteMember = ({ memberEmailList }: Props) => {
+  const team = useActiveTeam();
+  const teamMember = useUserTeamMember();
+  const supabaseClient = useSupabaseClient();
+  const [emailListData, setEmailListData] = useState<EmailListData>([]);
+  const [isInvitingMember, setIsInvitingMember] = useState(false);
+  const [emailList, setEmailList] = useState<string[]>([]);
+  const [pendingInviteList, setPendingInviteList] = useState<PendingInvite[]>(
+    []
+  );
+  const [isResendingInvite, setIsResendingInvite] = useState(false);
+
+  const handleInvite = async () => {
+    try {
+      if (!teamMember) return;
+      setIsInvitingMember(true);
+
+      await createTeamInvitation(supabaseClient, {
+        emailList,
+        teamMemberId: teamMember.team_member_id,
+        teamName: team.team_name,
+      });
+
+      sendEmailInvite(emailList);
+
+      setEmailList([]);
+      notifications.show({
+        message: "Team member/s invited.",
+        color: "green",
+      });
+    } catch {
+      notifications.show({
+        message: "Something went wrong. Please try again later.",
+        color: "red",
+      });
+    } finally {
+      setIsInvitingMember(false);
+    }
+  };
+
+  // send email invite to unregistered users
+  const sendEmailInvite = async (emailList: string[]) => {
+    const subject = `You have been invited to join ${team.team_name} on Formsly.`;
+    const html = `<p>Hi,</p>
+    <p>Please click the link below to accept the invitation.</p>
+    &nbsp;
+    <p><a href="${process.env.NEXT_PUBLIC_SITE_URL}/sign-up">${process.env.NEXT_PUBLIC_SITE_URL}/sign-up</a></p>
+    &nbsp;
+    <p>Thank you,</p>
+    <p>Formsly Team</p>`;
+
+    for (const email of emailList) {
+      const isEmailExists = await checkIfEmailExists(supabaseClient, {
+        email: email,
+      });
+      if (!isEmailExists) {
+        try {
+          const response = await axios.post("/api/send-email", {
+            to: email,
+            subject,
+            html,
+          });
+          return response.data;
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    }
+  };
+
+  const handleResendInvite = async (email: string) => {
+    try {
+      setIsResendingInvite(true);
+      await sendEmailInvite([email]);
+      setPendingInviteList((prev) =>
+        prev.map((pendingInvite) => {
+          if (pendingInvite.invitation_to_email === email) {
+            return {
+              ...pendingInvite,
+              is_resend_disabled: true,
+            };
+          } else {
+            return pendingInvite;
+          }
+        })
+      );
+      notifications.show({
+        message: "Invitation sent",
+        color: "green",
+      });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsResendingInvite(false);
+    }
+  };
+
+  useEffect(() => {
+    const fetchPendingTeamInvite = async () => {
+      const { data } = await getTeamInvitation(supabaseClient, {
+        teamId: team.team_id,
+        status: "PENDING",
+      });
+      setPendingInviteList(data as PendingInvite[]);
+    };
+
+    fetchPendingTeamInvite();
+  }, []);
 
   return (
     <Container p={0} mt="xl" pos="relative" fluid>
@@ -60,7 +174,7 @@ const InviteMember = ({
               creatable
               clearable
               clearSearchOnChange
-              onChange={onSetEmailList}
+              onChange={setEmailList}
               getCreateLabel={(query) => (
                 <Flex align="center" gap={4}>
                   <IconMailPlus size={14} />
@@ -96,7 +210,7 @@ const InviteMember = ({
 
             <Button
               onClick={() => {
-                onInviteMember();
+                handleInvite();
                 setEmailListData([]);
               }}
               leftIcon={<IconUsersPlus size={14} />}
@@ -106,6 +220,37 @@ const InviteMember = ({
             </Button>
           </Flex>
         </Stack>
+
+        <Box mt="lg">
+          <Text weight={600}>Pending Invites</Text>
+          <Stack mt="sm" fz={14} spacing="xs" pos="relative">
+            <LoadingOverlay visible={isResendingInvite} />
+            {pendingInviteList.map((invite) => {
+              const isResendDisabled = invite.is_resend_disabled;
+              return (
+                <Box key={invite.invitation_id}>
+                  <Divider />
+                  <Flex px="sm" mt="sm" justify="space-between" align="center">
+                    <Text>{invite.invitation_to_email}</Text>
+
+                    <Group position="right">
+                      <Button variant="subtle">Cancel</Button>
+                      <Button
+                        miw={84}
+                        onClick={() =>
+                          handleResendInvite(invite.invitation_to_email)
+                        }
+                        disabled={isResendDisabled}
+                      >
+                        {isResendDisabled ? "Sent" : "Resend"}
+                      </Button>
+                    </Group>
+                  </Flex>
+                </Box>
+              );
+            })}
+          </Stack>
+        </Box>
       </Paper>
     </Container>
   );
