2f4d2f88dc6da76beced5c110229bec6a35f0989
Juan Carlos Lumingkit <juancarloslumingkit@gmail.com>
2023-08-16 16:11:39 +0800

    update schema to add subscription

--- schema.sql
+++ schema.sql
@@ -2678,6 +2678,18 @@ CREATE INDEX request_list_idx ON request_table (request_id, request_date_created
 
 -------- End: INDEXES
 
+-------- Start: SUBSCRIPTION
+
+DROP PUBLICATION if exists supabase_realtime;
+
+CREATE PUBLICATION supabase_realtime;
+COMMIT;
+
+ALTER
+  PUBLICATION supabase_realtime ADD TABLE request_table, request_signer_table;
+
+-------- END: SUBSCRIPTION
+
 GRANT ALL ON ALL TABLES IN SCHEMA public TO PUBLIC;
 GRANT ALL ON ALL TABLES IN SCHEMA public TO POSTGRES;
 
