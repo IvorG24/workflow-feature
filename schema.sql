f551563ebfe07801ff4b76452f7daf7fa84d39f9
Juan Carlos Lumingkit <juancarloslumingkit@gmail.com>
2023-08-30 17:06:09 +0800

    update schema to add invitation_table, team_project_table, team_group_table

--- schema.sql
+++ schema.sql
@@ -3451,7 +3451,7 @@ DROP PUBLICATION if exists supabase_realtime;
 CREATE PUBLICATION supabase_realtime;
 COMMIT;
 
-ALTER PUBLICATION supabase_realtime ADD TABLE request_table, request_signer_table, comment_table, notification_table, team_member_table;
+ALTER PUBLICATION supabase_realtime ADD TABLE request_table, request_signer_table, comment_table, notification_table, team_member_table, invitation_table, team_project_table, team_group_table;
 
 -------- END: SUBSCRIPTION
 
