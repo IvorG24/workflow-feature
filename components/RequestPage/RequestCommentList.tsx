25412ab8baadd2a64d4aaff3b285ea6ea4201032
Juan Carlos Lumingkit <juancarloslumingkit@gmail.com>
2023-12-12 11:47:06 +0800

    fix comment link, remove team-request index

--- components/RequestPage/RequestCommentList.tsx
+++ components/RequestPage/RequestCommentList.tsx
@@ -11,6 +11,7 @@ import { RequestCommentType } from "@/utils/types";
 import { Divider, Group, Paper, Space, Stack, Title } from "@mantine/core";
 import { notifications } from "@mantine/notifications";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { FormProvider, useForm } from "react-hook-form";
 import { v4 as uuidv4 } from "uuid";
@@ -29,6 +30,7 @@ type Props = {
 
 const RequestCommentList = ({ requestData, requestCommentList }: Props) => {
   const userProfile = useUserProfile();
+  const router = useRouter();
   const teamMember = useUserTeamMember();
   const activeTeam = useActiveTeam();
   const supabaseClient = useSupabaseClient();
@@ -101,7 +103,7 @@ const RequestCommentList = ({ requestData, requestCommentList }: Props) => {
             notification_content: `${commenterFullName} commented on your request`,
             notification_redirect_url: `/${formatTeamNameToUrlKey(
               activeTeam.team_name ?? ""
-            )}/requests/${requestData.requestId}`,
+            )}/requests/${router.query.requestId}`,
             notification_user_id: requestData.requestOwnerId,
             notification_team_id: requestData.teamId,
           });
