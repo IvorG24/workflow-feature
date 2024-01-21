1d3ea7ff96aab3fc8cee87c1424f4ecdf6db5efa
Lance Andrei <lancejuat26@gmail.com>
2024-01-21 09:40:29 +0800

    home-page-redirection

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,27 @@
 import HomePage from "@/components/HomePage/HomePage";
 import Meta from "@/components/Meta/Meta";
+import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
+import { GetServerSideProps } from "next";
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const supabaseClient = createPagesServerClient(context);
+  const {
+    data: { user },
+  } = await supabaseClient.auth.getUser();
+
+  if (user) {
+    return {
+      redirect: {
+        destination: "/userActiveTeam",
+        permanent: false,
+      },
+    };
+  } else {
+    return {
+      props: {},
+    };
+  }
+};
 
 // Our Landing Page.
 const Page = () => {
