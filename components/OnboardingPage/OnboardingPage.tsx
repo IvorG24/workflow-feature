78844aff376d0e0174d25ec7f5bb57a02c549a01
Mark Jay Lunas <you@exagit config --global user.email markjay.lunas@gmail.com git config --global user.email you@exa>
2024-02-20 13:59:15 +0800

    Fix layout

--- components/OnboardingPage/OnboardingPage.tsx
+++ components/OnboardingPage/OnboardingPage.tsx
@@ -213,7 +213,7 @@ const OnboardingPage = ({ user }: Props) => {
           </Center>
 
           <form onSubmit={handleSubmit(handleOnboardUser)}>
-            <Grid columns={2} mt="sm">
+            <Grid columns={2}>
               <Grid.Col xs={2} sm={1}>
                 <TextInput
                   label="Email"
