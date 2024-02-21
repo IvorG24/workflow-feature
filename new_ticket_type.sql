6814f4e6223c0d4240b7fe71d7478264a61f9931
Mark Jay Lunas <you@exagit config --global user.email markjay.lunas@gmail.com git config --global user.email you@exa>
2024-02-21 12:20:33 +0800

    Add fetch ticket list update

--- new_ticket_type.sql
+++ new_ticket_type.sql
@@ -263,6 +263,111 @@ INSERT INTO storage.buckets (id, name) VALUES ('TICKET_ATTACHMENTS', 'TICKET_ATT
 
 UPDATE storage.buckets SET public = true;
 
+-- Start: Fetch ticket list
+
+CREATE OR REPLACE FUNCTION fetch_ticket_list(
+    input_data JSON
+)
+RETURNS JSON AS $$
+    let return_value
+    plv8.subtransaction(function(){
+      const {
+        teamId,
+        page,
+        limit,
+        requester,
+        approver,
+        status,
+        sort,
+        category,
+        search,
+      } = input_data;
+
+      const start = (page - 1) * limit;
+
+      const ticket_list = plv8.execute(
+        `
+          SELECT DISTINCT
+            ticket_table.*,
+            ticket_category_table.ticket_category
+          FROM ticket_table
+          INNER JOIN team_member_table ON ticket_requester_team_member_id = team_member_id
+          INNER JOIN ticket_category_table ON ticket_category_table.ticket_category_id = ticket_table.ticket_category_id 
+          WHERE team_member_team_id = '${teamId}'
+          ${requester}
+          ${approver}
+          ${status}
+          ${category}
+          ${search}
+          ORDER BY ticket_date_created ${sort} 
+          OFFSET ${start} ROWS FETCH FIRST ${limit} ROWS ONLY
+        `
+      );
+
+      const ticket_count = plv8.execute(
+        `
+          SELECT DISTINCT COUNT(*)
+          FROM ticket_table
+          INNER JOIN team_member_table ON ticket_table.ticket_requester_team_member_id = team_member_table.team_member_id
+          WHERE team_member_table.team_member_team_id = '${teamId}'
+          ${requester}
+          ${approver}
+          ${status}
+          ${category}
+          ${search}
+        `
+      )[0];
+
+      const ticket_data = ticket_list.map(ticket => {
+        const ticket_requester = plv8.execute(
+          `
+            SELECT 
+              team_member_table.team_member_id, 
+              user_table.user_id,
+              user_table.user_first_name,
+              user_table.user_last_name,
+              user_table.user_username,
+              user_table.user_avatar
+            FROM team_member_table
+            INNER JOIN user_table ON team_member_table.team_member_user_id = user_table.user_id
+            WHERE team_member_table.team_member_id = '${ticket.ticket_requester_team_member_id}'
+          `
+        )[0];
+        let ticket_approver = {}
+        if(ticket.ticket_approver_team_member_id){
+          ticket_approver = plv8.execute(
+            `
+              SELECT 
+                team_member_table.team_member_id, 
+                user_table.user_id,
+                user_table.user_first_name,
+                user_table.user_last_name,
+                user_table.user_username,
+                user_table.user_avatar
+              FROM team_member_table
+              INNER JOIN user_table ON team_member_table.team_member_user_id = user_table.user_id
+              WHERE team_member_table.team_member_id = '${ticket.ticket_approver_team_member_id}'
+            `
+          )[0];
+        }
+
+        return {
+          ...ticket,
+          ticket_requester,
+          ticket_approver,
+        }
+      });
+
+      returnData = {
+        data: ticket_data, 
+        count: Number(ticket_count.count)
+      };
+    });
+    return returnData
+$$ LANGUAGE plv8;
+
+-- End: Fetch ticket list
+
 -- Start: Incident report metrics
 
 CREATE OR REPLACE FUNCTION get_incident_report(
