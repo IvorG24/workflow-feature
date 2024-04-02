438d5b35d4936619c3a363da89349c0346e5519e
Lance Andrei <lancejuat26@gmail.com>
2024-04-02 16:23:54 +0800

    optimize public request page

--- pages/public-request/[requestId].tsx
+++ pages/public-request/[requestId].tsx
@@ -1,7 +1,14 @@
 import { getTeam, getUserActiveTeamId } from "@/backend/api/get";
 import { checkIfEmailExists } from "@/backend/api/post";
+import ItemRequestPage from "@/components/ItemRequestPage/ItemRequestPage";
 import Meta from "@/components/Meta/Meta";
+import OtherExpensesRequestPage from "@/components/OtherExpensesRequestPage/OtherExpensesRequestPage";
+import PEDEquipmentRequestPage from "@/components/PEDEquipmentRequestPage/PEDEquipmentRequestPage";
+import PEDItemRequestPage from "@/components/PEDItemRequestPage/PEDItemRequestPage";
+import PEDPartRequestPage from "@/components/PEDPartRequestPage/PEDPartRequestPage";
+import PaymentRequestPage from "@/components/PaymentRequestPage/PaymentRequestPage";
 import RequestPage from "@/components/RequestPage/RequestPage";
+import ServicesRequestPage from "@/components/ServicesRequestPage/ServicesRequestPage";
 import { formatTeamNameToUrlKey } from "@/utils/string";
 import { RequestWithResponseType } from "@/utils/types";
 import { Space } from "@mantine/core";
@@ -11,14 +18,15 @@ import { GetServerSideProps } from "next";
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const supabaseClient = createPagesServerClient(context);
   try {
-    const { data: requestData, error } = await supabaseClient.rpc(
-      "get_request",
+    const { data, error } = await supabaseClient.rpc(
+      "public_request_page_on_load",
       {
-        request_id: `${context.query.requestId}`,
+        input_data: {
+          requestId: context.query.requestId,
+        },
       }
     );
     if (error) throw error;
-
     // * 1. Check if there is user active session
     const {
       data: { session },
@@ -71,7 +79,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
           redirect: {
             destination: `/${formatTeamNameToUrlKey(
               activeTeam.team_name
-            )}/requests/${requestData.request_formsly_id}`,
+            )}/requests/${data.request.request_formsly_id}`,
             permanent: false,
           },
         };
@@ -79,9 +87,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     }
 
     return {
-      props: {
-        request: requestData,
-      },
+      props: data,
     };
   } catch (e) {
     console.error(e);
@@ -96,18 +102,71 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
 type Props = {
   request: RequestWithResponseType;
+  duplicatableSectionIdList: string[];
 };
 
-const Page = ({ request }: Props) => {
+const Page = ({ request, duplicatableSectionIdList }: Props) => {
+  const formslyForm = () => {
+    if (request.request_form.form_name === "Item") {
+      return (
+        <ItemRequestPage
+          request={request}
+          duplicatableSectionIdList={duplicatableSectionIdList}
+        />
+      );
+    } else if (request.request_form.form_name === "Services") {
+      return (
+        <ServicesRequestPage
+          request={request}
+          duplicatableSectionIdList={duplicatableSectionIdList}
+        />
+      );
+    } else if (request.request_form.form_name === "Other Expenses") {
+      return (
+        <OtherExpensesRequestPage
+          request={request}
+          duplicatableSectionIdList={duplicatableSectionIdList}
+        />
+      );
+    } else if (request.request_form.form_name === "PED Equipment") {
+      return (
+        <PEDEquipmentRequestPage
+          request={request}
+          duplicatableSectionIdList={duplicatableSectionIdList}
+        />
+      );
+    } else if (request.request_form.form_name === "PED Part") {
+      return (
+        <PEDPartRequestPage
+          request={request}
+          duplicatableSectionIdList={duplicatableSectionIdList}
+        />
+      );
+    } else if (request.request_form.form_name === "PED Item") {
+      return (
+        <PEDItemRequestPage
+          request={request}
+          duplicatableSectionIdList={duplicatableSectionIdList}
+        />
+      );
+    } else if (request.request_form.form_name === "Request For Payment") {
+      return <PaymentRequestPage request={request} />;
+    } else {
+      return <RequestPage request={request} isFormslyForm />;
+    }
+  };
+
   return (
     <>
-      <Meta description="Request Page" url="/<teamName>/requests/[requestId]" />
-      <Space h="xl" />
-      <RequestPage
-        request={request}
-        isAnon={true}
-        isFormslyForm={request.request_form.form_is_formsly_form}
+      <Meta
+        description="Public Request Page"
+        url="/public-request/[requestId]"
       />
+      <Space h="xl" />
+      {request.request_form.form_is_formsly_form ? formslyForm() : null}
+      {!request.request_form.form_is_formsly_form ? (
+        <RequestPage request={request} />
+      ) : null}
       <Space h="xl" />
     </>
   );
