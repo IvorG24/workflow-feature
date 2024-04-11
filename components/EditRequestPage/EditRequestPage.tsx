4ccd42eb41a4bc602cac9077acd2938fdf0a455c
Lance Andrei <lancejuat26@gmail.com>
2024-04-11 11:28:32 +0800

    fix build

--- components/EditRequestPage/EditRequestPage.tsx
+++ components/EditRequestPage/EditRequestPage.tsx
@@ -1,11 +1,5 @@
-import {
-  getNonDuplictableSectionResponse,
-  getProjectSignerWithTeamMember,
-} from "@/backend/api/get";
+import { getNonDuplictableSectionResponse } from "@/backend/api/get";
 import { createRequest, editRequest } from "@/backend/api/post";
-import RequestFormDetails from "@/components/CreateRequestPage/RequestFormDetails";
-import RequestFormSection from "@/components/CreateRequestPage/RequestFormSection";
-import RequestFormSigner from "@/components/CreateRequestPage/RequestFormSigner";
 import { useLoadingActions } from "@/stores/useLoadingStore";
 import { useActiveTeam } from "@/stores/useTeamStore";
 import { useUserProfile, useUserTeamMember } from "@/stores/useUserStore";
@@ -15,7 +9,6 @@ import { formatTeamNameToUrlKey } from "@/utils/string";
 import {
   FormType,
   FormWithResponseType,
-  OptionTableRow,
   RequestResponseTableRow,
   RequestTableRow,
 } from "@/utils/types";
@@ -33,40 +26,48 @@ import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { FormProvider, useFieldArray, useForm } from "react-hook-form";
+import { v4 as uuidv4 } from "uuid";
+import RequestFormDetails from "../CreateRequestPage/RequestFormDetails";
+import RequestFormSection from "../CreateRequestPage/RequestFormSection";
+import RequestFormSigner from "../CreateRequestPage/RequestFormSigner";
 
 export type Section = FormWithResponseType["form_section"][0];
-export type Field = FormType["form_section"][0]["section_field"][0];
 
 export type RequestFormValues = {
   sections: Section[];
 };
 
-export type FieldWithResponseArray = Field & {
-  field_response: RequestResponseTableRow[];
-};
+export type FieldWithResponseArray =
+  FormType["form_section"][0]["section_field"][0] & {
+    field_response: RequestResponseTableRow[];
+  };
 
 type Props = {
   form: FormType;
-  projectOptions: OptionTableRow[];
   requestId: string;
-
+  duplicatableSectionIdList: string[];
+  formslyFormName?: string;
+  requestProjectId?: string;
 };
 
-const EditRequestForPaymentPage = ({
+const EditRequestPage = ({
   form,
-  projectOptions,
   requestId,
-
+  formslyFormName = "",
+  requestProjectId,
 }: Props) => {
   const router = useRouter();
   const supabaseClient = createPagesBrowserClient<Database>();
   const teamMember = useUserTeamMember();
-  const requestorProfile = useUserProfile();
   const team = useActiveTeam();
 
   const isReferenceOnly = Boolean(router.query.referenceOnly);
 
+  const requestorProfile = useUserProfile();
   const { setIsLoading } = useLoadingActions();
+
+  const [initialRequestDetails, setInitialRequestDetails] =
+    useState<RequestFormValues>();
 
   const formDetails = {
     form_name: form.form_name,
@@ -74,50 +75,29 @@ const EditRequestForPaymentPage = ({
     form_date_created: form.form_date_created,
     form_team_member: form.form_team_member,
   };
+  const signerList = form.form_signer.map((signer) => ({
+    ...signer,
+    signer_action: signer.signer_action.toUpperCase(),
+  }));
 
-  const requestFormMethods = useForm<RequestFormValues>({ mode: "onChange" });
-  const { handleSubmit, control, setValue, getValues } = requestFormMethods;
+  const requestFormMethods = useForm<RequestFormValues>();
+  const { handleSubmit, control, getValues } = requestFormMethods;
   const {
     fields: formSections,
-    replace: replaceSection,
-    update: updateSection,
+    insert: addSection,
     remove: removeSection,
-    insert: insertSection,
+    replace: replaceSection,
   } = useFieldArray({
     control,
     name: "sections",
   });
 
-  const [initialRequestDetails, setInitialRequestDetails] =
-    useState<RequestFormValues>();
-  const [isFetchingSigner, setIsFetchingSigner] = useState(false);
-  const [signerList, setSignerList] = useState(
-    form.form_signer.map((signer) => ({
-      ...signer,
-      signer_action: signer.signer_action.toUpperCase(),
-    }))
-  );
-
   const onSubmit = async (data: RequestFormValues) => {
-    if (isFetchingSigner) {
-      notifications.show({
-        message: "Wait until all signers are fetched before submitting.",
-        color: "orange",
-      });
-      return;
-    }
     try {
       if (!requestorProfile) return;
       if (!teamMember) return;
 
       setIsLoading(true);
-
-      const response = data.sections[0].section_field[0]
-        .field_response as string;
-
-      const projectId = data.sections[0].section_field[0].field_option.find(
-        (option) => option.option_value === response
-      )?.option_id as string;
 
       let request: RequestTableRow;
       if (isReferenceOnly) {
@@ -125,19 +105,19 @@ const EditRequestForPaymentPage = ({
           requestFormValues: data,
           formId: form.form_id,
           teamMemberId: teamMember.team_member_id,
-          signers: signerList,
+          signers: form.form_signer,
           teamId: teamMember.team_member_team_id,
           requesterName: `${requestorProfile.user_first_name} ${requestorProfile.user_last_name}`,
           formName: form.form_name,
-          isFormslyForm: true,
-          projectId,
+          isFormslyForm: false,
+          projectId: requestProjectId || "",
           teamName: formatTeamNameToUrlKey(team.team_name ?? ""),
         });
       } else {
         request = await editRequest(supabaseClient, {
           requestId,
           requestFormValues: data,
-          signers: signerList,
+          signers: form.form_signer,
           teamId: teamMember.team_member_team_id,
           requesterName: `${requestorProfile.user_first_name} ${requestorProfile.user_last_name}`,
           formName: form.form_name,
@@ -149,11 +129,10 @@ const EditRequestForPaymentPage = ({
         message: `Request ${isReferenceOnly ? "created" : "edited"}.`,
         color: "green",
       });
-
       router.push(
         `/${formatTeamNameToUrlKey(team.team_name ?? "")}/requests/${
-          request.request_formsly_id_prefix
-        }-${request.request_formsly_id_serial}`
+          request.request_id
+        }`
       );
     } catch (error) {
       notifications.show({
@@ -165,68 +144,39 @@ const EditRequestForPaymentPage = ({
     }
   };
 
-  const resetSigner = () => {
-    setSignerList(
-      form.form_signer.map((signer) => ({
-        ...signer,
-        signer_action: signer.signer_action.toUpperCase(),
-      }))
+  const handleDuplicateSection = (sectionId: string) => {
+    const sectionLastIndex = formSections
+      .map((sectionItem) => sectionItem.section_id)
+      .lastIndexOf(sectionId);
+    const sectionMatch = form.form_section.find(
+      (section) => section.section_id === sectionId
     );
-  };
-
-  const handleProjectNameChange = async (value: string | null) => {
-    try {
-      setIsFetchingSigner(true);
-      if (value) {
-        const projectId = projectOptions.find(
-          (option) => option.option_value === value
-        )?.option_id;
-        if (projectId) {
-          const data = await getProjectSignerWithTeamMember(supabaseClient, {
-            projectId,
-            formId: form.form_id,
-          });
-          if (data.length !== 0) {
-            setSignerList(data as unknown as FormType["form_signer"]);
-          } else {
-            resetSigner();
-          }
-        }
-      } else {
-        resetSigner();
-      }
-    } catch (e) {
-      setValue(`sections.0.section_field.0.field_response`, "");
-      notifications.show({
-        message: "Something went wrong. Please try again later.",
-        color: "red",
-      });
-    } finally {
-      setIsFetchingSigner(false);
+    if (sectionMatch) {
+      const sectionDuplicatableId = uuidv4();
+      const duplicatedFieldsWithDuplicatableId = sectionMatch.section_field.map(
+        (field) => ({
+          ...field,
+          field_section_duplicatable_id: sectionDuplicatableId,
+        })
+      );
+      const newSection = {
+        ...sectionMatch,
+        section_field: duplicatedFieldsWithDuplicatableId,
+      };
+      addSection(sectionLastIndex + 1, newSection);
+      return;
     }
   };
 
-  const handleRequestTypeChange = async (
-    value: string | null,
-    index: number
-  ) => {
-    const defaultSection = form.form_section[index];
-    const currentSection = getValues(`sections.${index}`);
-    const defaultPoFieldIndex = defaultSection.section_field.findIndex(
-      (field) => field.field_name === "PO Number"
+  const handleRemoveSection = (sectionDuplicatableId: string) => {
+    const sectionMatchIndex = formSections.findIndex(
+      (section) =>
+        section.section_field[0].field_section_duplicatable_id ===
+        sectionDuplicatableId
     );
-    const poFieldExists =
-      currentSection.section_field[defaultPoFieldIndex].field_name ===
-      "PO Number";
-
-    if (value === "With PO" && !poFieldExists) {
-      const poFieldValue = defaultSection.section_field[defaultPoFieldIndex];
-      currentSection.section_field.splice(defaultPoFieldIndex, 0, poFieldValue);
-      updateSection(index, currentSection);
-    } else if ((value === "Without PO" && poFieldExists) || !value) {
-      currentSection.section_field.splice(defaultPoFieldIndex, 1);
-      removeSection(index);
-      insertSection(index, currentSection);
+    if (sectionMatchIndex) {
+      removeSection(sectionMatchIndex);
+      return;
     }
   };
 
@@ -237,19 +187,23 @@ const EditRequestForPaymentPage = ({
       const fetchRequestDetails = async () => {
         // Fetch response
         const fieldIdList: string[] = [];
-        form.form_section.forEach((section) =>
+        form.form_section.forEach((section) => {
+          if (section.section_is_duplicatable) return;
           section.section_field.map((field) => {
             fieldIdList.push(field.field_id);
-          })
-        );
-        const nonDuplicatableSectionResponse =
-          await getNonDuplictableSectionResponse(supabaseClient, {
+          });
+        });
+        const sectionResponse = await getNonDuplictableSectionResponse(
+          supabaseClient,
+          {
             requestId,
             fieldIdList,
-          });
-        const requestDetailsSection = form.form_section[0].section_field
-          .map((field) => {
-            const response = nonDuplicatableSectionResponse.find(
+          }
+        );
+
+        const requestDetailsSection = form.form_section.map((section) => {
+          const sectionField = section.section_field.map((field) => {
+            const response = sectionResponse.find(
               (response) =>
                 response.request_response_field_id === field.field_id
             );
@@ -259,31 +213,15 @@ const EditRequestForPaymentPage = ({
                 ? safeParse(response.request_response)
                 : "",
             };
-          })
-          .filter((field) => field.field_response);
-        const payeeSection = form.form_section[1].section_field.map((field) => {
-          const response = nonDuplicatableSectionResponse.find(
-            (response) => response.request_response_field_id === field.field_id
-          );
+          });
           return {
-            ...field,
-            field_response: response
-              ? safeParse(response.request_response)
-              : "",
+            ...section,
+            section_field: sectionField,
           };
         });
-        const finalInitialRequestDetails = [
-          {
-            ...form.form_section[0],
-            section_field: requestDetailsSection,
-          },
-          {
-            ...form.form_section[1],
-            section_field: payeeSection,
-          },
-        ];
-        replaceSection(finalInitialRequestDetails);
-        setInitialRequestDetails({ sections: finalInitialRequestDetails });
+
+        replaceSection(requestDetailsSection);
+        setInitialRequestDetails({ sections: requestDetailsSection });
         setIsLoading(false);
       };
       fetchRequestDetails();
@@ -299,10 +237,6 @@ const EditRequestForPaymentPage = ({
 
   const handleResetRequest = () => {
     replaceSection(initialRequestDetails ? initialRequestDetails.sections : []);
-    handleProjectNameChange(
-      initialRequestDetails?.sections[0].section_field[0]
-        .field_response as string
-    );
   };
 
   return (
@@ -316,19 +250,36 @@ const EditRequestForPaymentPage = ({
           <Stack spacing="xl">
             <RequestFormDetails formDetails={formDetails} />
             {formSections.map((section, idx) => {
+              // used to render add duplicate button
+              // find the last index of current section, and render add duplicate button if match
+              const sectionIdToFind = section.section_id;
+              const sectionLastIndex = getValues("sections")
+                .map((sectionItem) => sectionItem.section_id)
+                .lastIndexOf(sectionIdToFind);
+
               return (
                 <Box key={section.id}>
                   <RequestFormSection
                     key={section.section_id}
                     section={section}
                     sectionIndex={idx}
-                    formslyFormName={form.form_name}
-                    paymentRequestFormMethods={{
-                      onProjectNameChange: handleProjectNameChange,
-                      onRequestTypeChange: handleRequestTypeChange,
-                    }}
+                    onRemoveSection={handleRemoveSection}
+                    formslyFormName={formslyFormName}
                     isEdit={!isReferenceOnly}
                   />
+                  {section.section_is_duplicatable &&
+                    idx === sectionLastIndex && (
+                      <Button
+                        mt="md"
+                        variant="default"
+                        onClick={() =>
+                          handleDuplicateSection(section.section_id)
+                        }
+                        fullWidth
+                      >
+                        {section.section_name} +
+                      </Button>
+                    )}
                 </Box>
               );
             })}
@@ -350,4 +301,4 @@ const EditRequestForPaymentPage = ({
   );
 };
 
-export default EditRequestForPaymentPage;
+export default EditRequestPage;
