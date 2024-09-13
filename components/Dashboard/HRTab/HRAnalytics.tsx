f6d6d4bb46465cbc6562a77762e67520eba41711
Juan Carlos Lumingkit <juancarloslumingkit@gmail.com>
2024-09-13 18:38:20 +0800

    add chart

--- components/Dashboard/HRTab/HRAnalytics.tsx
+++ components/Dashboard/HRTab/HRAnalytics.tsx
@@ -1,6 +1,9 @@
 import { getCandidateReferralSource } from "@/backend/api/get";
-import { Container } from "@mantine/core";
+import PieChart from "@/components/Chart/PieChart";
+import { parseDataForChart, safeParse } from "@/utils/functions";
+import { Container, Paper } from "@mantine/core";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { ChartData } from "chart.js";
 import { useEffect, useState } from "react";
 
 type CandidateReferralSourceType = {
@@ -22,7 +25,26 @@ const HRAnalytics = () => {
     fetchHRAnalyticsData();
   }, []);
 
-  return <Container p={0}></Container>;
+  return (
+    <Container p={0}>
+      {candidateReferralSourceData ? (
+        <Paper w={{ base: 300, lg: 600 }} p="md" withBorder>
+          <PieChart
+            data={
+              parseDataForChart({
+                data: candidateReferralSourceData.map((d) => ({
+                  ...d,
+                  request_response: safeParse(d.request_response),
+                })),
+                labelPropKey: "request_response",
+                valuePropKey: "count",
+              }) as ChartData<"pie">
+            }
+          />
+        </Paper>
+      ) : null}
+    </Container>
+  );
 };
 
 export default HRAnalytics;
