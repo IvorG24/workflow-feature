608f7634502c865b6805a2c26ea7e5bcc11da3db
Ivor <lhanivor@gmail.com>
2024-09-20 16:54:11 +0800

    close the drawer for spreadsheet filter

--- components/ApplicationInformationSpreadsheetView/ApplicationInformationFilterMenu.tsx
+++ components/ApplicationInformationSpreadsheetView/ApplicationInformationFilterMenu.tsx
@@ -494,7 +494,10 @@ const ApplicationInformationFilterMenu = ({
         scrollAreaComponent={ScrollArea.Autosize}
       >
         <form
-          onSubmit={handleSubmit((data) => fetchData({ ...data, page: 1 }))}
+          onSubmit={handleSubmit((data) => {
+            fetchData({ ...data, page: 1 });
+            closeFilterMenu();
+          })}
         >
           <Stack spacing="xs" sx={{ overflow: "hidden" }}>
             <Accordion variant="contained">
